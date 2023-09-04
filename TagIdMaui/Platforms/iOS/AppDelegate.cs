a502d5bfd58bba3ebb7e5a790e3ef9c86353a4a9
Ianos Gnatiuc <ignatiuc@netinfo.md>
2023-09-05 01:33:41 +0300

    Initial MAUI project

---
+++ TagIdMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TagIdMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
