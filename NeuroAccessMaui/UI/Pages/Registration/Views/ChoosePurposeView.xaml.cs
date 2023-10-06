716a61ec5056551012fd1e4edb591354dff5a777
Ianos Gnatiuc <ignatiuc@netinfo.md>
2023-10-06 23:43:15 +0300

    Show purposes in a stack layout instead of a collection view

--- NeuroAccessMaui/UI/Pages/Registration/Views/ChoosePurposeView.xaml.cs
+++ NeuroAccessMaui/UI/Pages/Registration/Views/ChoosePurposeView.xaml.cs
@@ -1,3 +1,7 @@
+using AndroidX.Lifecycle;
+using CommunityToolkit.Mvvm.Input;
+using NeuroAccessMaui.Services.Tag;
+
 namespace NeuroAccessMaui.Pages.Registration.Views;
 
 public partial class ChoosePurposeView
@@ -7,9 +11,57 @@ public partial class ChoosePurposeView
 		return Create<ChoosePurposeView>();
 	}
 
+	private ChoosePurposeViewModel viewModel;
+
 	public ChoosePurposeView(ChoosePurposeViewModel ViewModel)
 	{
 		this.InitializeComponent();
 		this.ContentViewModel = ViewModel;
+		this.viewModel = ViewModel;
+	}
+
+	[RelayCommand]
+	public void SelectPurpose(object o)
+	{
+		if (o is not PurposeUse Purpose)
+		{
+			return;
+		}
+
+		foreach (object Item in this.PurposesContainer)
+		{
+			if ((Item is VisualElement Element) &&
+				(Element.BindingContext is PurposeInfo PurposeInfo))
+			{
+				if (Purpose == PurposeInfo.Purpose)
+				{
+					VisualStateManager.GoToState(Element, VisualStateManager.CommonStates.Selected);
+					this.viewModel.SelectedPurpose = PurposeInfo;
+				}
+				else
+				{
+					VisualStateManager.GoToState(Element, VisualStateManager.CommonStates.Normal);
+				}
+			}
+		}
+	}
+
+	[RelayCommand]
+	public void ShowPurposeInfo(object o)
+	{
+		if (o is not PurposeUse Purpose)
+		{
+			return;
+		}
+
+		foreach (object Item in this.PurposesContainer)
+		{
+			if ((Item is VisualElement Element) &&
+				(Element.BindingContext is PurposeInfo PurposeInfo) &&
+				(Purpose == PurposeInfo.Purpose))
+			{
+				break;
+			}
+		}
 	}
 }
