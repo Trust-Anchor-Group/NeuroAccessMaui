a1b974991ed728303aac7330f518d214df344809
Ianos Gnatiuc <ignatiuc@netinfo.md>
2023-11-02 20:57:25 +0200

    Fix the main activity

--- NeuroAccessMaui/Platforms/Android/MainActivity.cs
+++ NeuroAccessMaui/Platforms/Android/MainActivity.cs
@@ -1,9 +1,18 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
+using Android.Nfc;
 
 namespace NeuroAccessMaui;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+	ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density | ConfigChanges.Locale,
+	ScreenOrientation = ScreenOrientation.Portrait, LaunchMode = LaunchMode.SingleTop)]
+[IntentFilter([NfcAdapter.ActionNdefDiscovered],
+		Categories = [Intent.CategoryDefault], DataMimeType = "*/*")]
+[IntentFilter([Intent.ActionView],
+		Categories = [Intent.CategoryDefault, Intent.CategoryBrowsable],
+		DataSchemes = ["iotid", "tagsign", "obinfo"])]
 public class MainActivity : MauiAppCompatActivity
 {
 }
