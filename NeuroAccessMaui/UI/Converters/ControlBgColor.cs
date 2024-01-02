3ab30d16432382807f2aa6165989490669c7894b
Peter Waher <peterwaher@hotmail.com>
2024-01-02 18:33:12 -0300

    Converter for backgtround colors of input fields

---
+++ NeuroAccessMaui/UI/Converters/ControlBgColor.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using NeuroAccessMaui.Services;
+
+namespace NeuroAccessMaui.UI.Converters
+{
+	/// <summary>
+	/// Converts an input control OK state to a background color.
+	/// </summary>
+	public class ControlBgColor : IValueConverter, IMarkupExtension
+	{
+		/// <inheritdoc/>
+		public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if (value is bool Ok)
+				return ToColor(Ok);
+			else if (value is Color Color)
+				return Color;
+			else
+				return Colors.Transparent;
+		}
+
+		/// <summary>
+		/// Converts a contract state to a representative color.
+		/// </summary>
+		/// <param name="State">Contract State</param>
+		/// <returns>Color</returns>
+		public static Color ToColor(bool Ok)
+		{
+			return Ok switch
+			{
+				true => Colors.Transparent,
+				false => ServiceRef.TagProfile.Theme == AppTheme.Light ? Colors.Salmon : Colors.LightSalmon,
+			};
+		}
+
+		/// <inheritdoc/>
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			return value;
+		}
+
+		/// <inheritdoc/>
+		public object ProvideValue(System.IServiceProvider serviceProvider)
+		{
+			return this;
+		}
+	}
+}
