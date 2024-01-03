22430ac4607143b3bf0d4b150683acede704e766
Peter Waher <peterwaher@hotmail.com>
2024-01-03 11:32:59 -0300

    Behaviour to hide keyboard when done with last field in a form.

---
+++ NeuroAccessMaui/UI/Behaviors/HideKeyboardOnCompletedBehavior.cs
@@ -0,0 +1,30 @@
+﻿using NeuroAccessMaui.Services;
+using NeuroAccessMaui.UI.Controls;
+
+namespace NeuroAccessMaui.UI.Behaviors
+{
+	/// <summary>
+	/// Used for hiding keyboard once the user hits the Enter or Return key.
+	/// </summary>
+	public class HideKeyboardOnCompletedBehavior : Behavior<CompositeEntry>
+	{
+		/// <inheritdoc/>
+		protected override void OnAttachedTo(CompositeEntry entry)
+		{
+			entry.Completed += this.Entry_Completed;
+			base.OnAttachedTo(entry);
+		}
+
+		/// <inheritdoc/>
+		protected override void OnDetachingFrom(CompositeEntry entry)
+		{
+			entry.Completed -= this.Entry_Completed;
+			base.OnDetachingFrom(entry);
+		}
+
+		private void Entry_Completed(object? Sender, EventArgs e)
+		{
+			ServiceRef.PlatformSpecific.HideKeyboard();
+		}
+	}
+}
