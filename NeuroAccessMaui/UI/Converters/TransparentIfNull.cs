06e652cc612021ff524a0c291db9ed43702408ca
Peter Waher <peterwaher@hotmail.com>
2024-01-13 18:33:24 -0300

    Hiding date if no date selected

---
+++ NeuroAccessMaui/UI/Converters/TransparentIfNull.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using NeuroAccessMaui.Services;
+
+namespace NeuroAccessMaui.UI.Converters
+{
+	/// <summary>
+	/// Transparent color, if value is null.
+	/// </summary>
+	public class TransparentIfNull : IValueConverter, IMarkupExtension
+	{
+		/// <inheritdoc/>
+		public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if (value is null)
+				return Colors.Transparent;
+
+			try
+			{
+				string Key = ServiceRef.TagProfile.Theme == AppTheme.Light ? "NormalEditForegroundLight" : "NormalEditForegroundDark";
+				if (!(App.Current?.Resources.TryGetValue(Key, out object Obj) ?? false))
+					return Colors.Transparent;
+
+				if (Obj is Color Color)
+					return Color;
+				else
+					return Colors.Transparent;
+			}
+			catch (Exception)
+			{
+				return Colors.Transparent;
+			}
+		}
+
+		/// <inheritdoc/>
+		public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			return value;
+		}
+
+		/// <inheritdoc/>
+		public object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return this;
+		}
+	}
+}
