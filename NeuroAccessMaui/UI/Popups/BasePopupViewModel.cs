7bccd9507333d0f800c724ea64706f315a0acc6b
berggrenmille <berggrenmille@hotmail.se>
2024-02-14 14:21:22 +0100

    Added Base Classes for Popup ViewModels

---
+++ NeuroAccessMaui/UI/Popups/BasePopupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuroAccessMaui.UI.Pages;
+
+namespace NeuroAccessMaui.UI.Popups
+{
+	public class BasePopupViewModel : BaseViewModel
+	{
+		public virtual void Close()
+		{
+		}
+	}
+}
