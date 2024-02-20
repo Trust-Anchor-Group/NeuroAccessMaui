034f3ea328c32df56f405bff6d817c3a96733fbb
Maximiliam Berggren <berggrenmille@hotmail.se>
2024-02-20 12:48:31 +0100

    Popup Migration work
    Created a refactored ImageView called ImagePopup.
    
    Updated ViewIdentityPage to use the new ImagePopup

---
+++ NeuroAccessMaui/UI/Popups/Image/ImagesViewModel.cs
@@ -0,0 +1,61 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NeuroAccessMaui.Services;
+using NeuroAccessMaui.Services.UI.Photos;
+using System.Collections.ObjectModel;
+using Waher.Networking.XMPP.Contracts;
+
+namespace NeuroAccessMaui.UI.Popups.Image
+{
+
+
+	public partial class ImagesViewModel : BasePopupViewModel
+	{
+		private readonly PhotosLoader photosLoader;
+
+
+		public ImagesViewModel()
+		{
+			this.Photos = [];
+			this.photosLoader = new PhotosLoader(this.Photos);
+		}
+		/// <summary>
+		/// Holds the list of photos to display.
+		/// </summary>
+		public ObservableCollection<Photo> Photos { get; }
+
+		/// <summary>
+		/// Gets or sets whether a user can swipe to see the photos.
+		/// </summary>
+		[ObservableProperty]
+		private bool isSwipeEnabled;
+
+		/// <summary>
+		/// Loads the attachments photos, if there are any.
+		/// </summary>
+		/// <param name="attachments">The attachments to load.</param>
+		public void LoadPhotos(Attachment[] attachments)
+		{
+			this.photosLoader.CancelLoadPhotos();
+			this.IsSwipeEnabled = false;
+
+			_ = this.photosLoader.LoadPhotos(attachments, SignWith.LatestApprovedIdOrCurrentKeys, () =>
+			{
+				MainThread.BeginInvokeOnMainThread(() => this.IsSwipeEnabled = this.Photos.Count > 1);
+			});
+		}
+
+		public override void OnPop()
+		{
+			this.photosLoader.CancelLoadPhotos();
+		}
+		/// <summary>
+		/// Cancels
+		/// </summary>
+		[RelayCommand]
+		private async Task Cancel()
+		{
+			await ServiceRef.PopupService.PopAsync();
+		}
+	}
+}
