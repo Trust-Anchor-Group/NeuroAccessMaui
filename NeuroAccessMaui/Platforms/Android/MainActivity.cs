5cd8c9cf749eedffdae9d6b272cf641cc16f4b8f
Maximiliam Berggren <maximiliam.berggren@trustanchorgroup.com>
2024-04-26 14:52:58 +0200

    Progress for #19
    #19 Now catches the exception and logs it. Have not been able to reproduce issue.

--- NeuroAccessMaui/Platforms/Android/MainActivity.cs
+++ NeuroAccessMaui/Platforms/Android/MainActivity.cs
@@ -1,7 +1,9 @@
-﻿using Android.App;
+﻿using System.Text;
+using Android.App;
 using Android.Content;
 using Android.Content.PM;
 using Android.Nfc;
+using Android.OS;
 
 namespace NeuroAccessMaui
 {
@@ -15,5 +17,27 @@ namespace NeuroAccessMaui
 			DataSchemes = ["iotid", "iotdisco", "iotsc", "tagsign", "obinfo", "edaler", "nfeat", "xmpp", "aes256", "neuroaccess"])]
 	public class MainActivity : MauiAppCompatActivity
 	{
+
+		protected override void OnPostCreate(Bundle? savedInstanceState)
+		{
+			try
+			{
+				base.OnPostCreate(savedInstanceState);
+			}
+			catch (Exception ex)
+			{
+					StringBuilder msg = new();
+
+					msg.AppendLine("An error occurred in the Android MainActivity.OnPostCreate method.");
+					msg.AppendLine("Exception message:");
+					msg.Append(ex.Message);
+					msg.AppendLine();
+					msg.AppendLine("```");
+					msg.AppendLine(ex.StackTrace);
+					msg.AppendLine("```");
+
+					App.SendAlert(msg.ToString(), "text/plain").Wait();
+			}
+		}
 	}
 }
