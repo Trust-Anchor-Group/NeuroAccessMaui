8cdafed0e3f3d35adba606bc41aec33683a3651a
Maximiliam Berggren <maximiliam.berggren@trustanchorgroup.com>
2024-06-10 11:27:09 +0200

    Keyboard now adapts to Password Type

--- NeuroAccessMaui/UI/Popups/Password/CheckPasswordPopup.xaml.cs
+++ NeuroAccessMaui/UI/Popups/Password/CheckPasswordPopup.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace NeuroAccessMaui.UI.Popups.Password
+﻿using NeuroAccessMaui.Services;
+
+namespace NeuroAccessMaui.UI.Popups.Password
 {
 	/// <summary>
 	/// A Popup letting the user enter a password to be verified with the password defined by the user earlier.
@@ -8,6 +10,7 @@
 		public CheckPasswordPopup()
 		{
 			this.InitializeComponent();
+			this.PasswordEntry.Keyboard = ServiceRef.TagProfile.IsNumericPassword ? Keyboard.Telephone : Keyboard.Default;
 		}
 
 		protected override void OnAppearing()
