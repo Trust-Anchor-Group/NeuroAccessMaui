123968e08ab0c656a6d4230c72d1a0aa7a38e6c3
berggrenmille <berggrenmille@hotmail.se>
2024-06-26 23:52:27 +0200

    Fixed an issue with popups and alerts
    Displaying an alert during the popping of an alert would close the alert

--- NeuroAccessMaui/UI/Popups/Password/CheckPasswordViewModel.cs
+++ NeuroAccessMaui/UI/Popups/Password/CheckPasswordViewModel.cs
@@ -1,8 +1,13 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using NeuroAccessMaui.Resources.Languages;
 using NeuroAccessMaui.Services;
 using NeuroAccessMaui.UI.Pages.Registration.Views;
+using Waher.Content;
+using Waher.Script.Constants;
 
 namespace NeuroAccessMaui.UI.Popups.Password
 {
@@ -11,6 +16,7 @@ namespace NeuroAccessMaui.UI.Popups.Password
 	/// </summary>
 	public partial class CheckPasswordViewModel(AuthenticationPurpose purpose) : ReturningPopupViewModel<string>
 	{
+
 		/// <summary>
 		/// Password text entry
 		/// </summary>
@@ -26,7 +32,7 @@ namespace NeuroAccessMaui.UI.Popups.Password
 		/// <summary>
 		/// The purpose of the authentication request.
 		/// </summary>
-		public AuthenticationPurpose PurposeInfo { get ; } = purpose;
+		public AuthenticationPurpose PurposeInfo { get; } = purpose;
 
 		/// <summary>
 		/// Enters the password provided by the user.
@@ -40,8 +46,8 @@ namespace NeuroAccessMaui.UI.Popups.Password
 
 				if (await App.CheckPasswordAndUnblockUser(Password))
 				{
-					this.result.TrySetResult(Password);
 					await ServiceRef.UiService.PopAsync();
+					this.result.TrySetResult(Password);
 				}
 				else
 				{
@@ -65,8 +71,82 @@ namespace NeuroAccessMaui.UI.Popups.Password
 		[RelayCommand]
 		private async Task Cancel()
 		{
-			this.result.TrySetResult(null);
 			await ServiceRef.UiService.PopAsync();
+			this.result.TrySetResult(null);
 		}
 	}
+	///TODO: remove or implement this
+	/* Recover password
+	 	try
+	   {
+	   	string AcceptLanguage = App.SelectedLanguage.TwoLetterISOLanguageName;
+
+	   	if (AcceptLanguage != "en")
+	   		AcceptLanguage += ";q=1,en;q=0.9";
+
+	   	byte[] nonceData = new byte[32];
+
+	   	using (RandomNumberGenerator Rnd = RandomNumberGenerator.Create())
+	   	{
+	   		Rnd.GetBytes(nonceData);
+	   	}
+	   	string nonce = Guid.NewGuid().ToString("n");
+	   	string host = ServiceRef.TagProfile.Domain;
+	   	string s = $"{ServiceRef.TagProfile.Account}:{host}:{nonce}";
+	   	string NewNetworkPassword = ServiceRef.CryptoService.CreateRandomPassword();
+	   	if (!await ServiceRef.XmppService.ChangePassword(NewNetworkPassword))
+	   		throw new Exception("password failed");
+	   	ServiceRef.TagProfile.SetAccount(ServiceRef.TagProfile.Account!, NewNetworkPassword, string.Empty);
+
+	   	byte[] keyBytes = Encoding.UTF8.GetBytes(NewNetworkPassword);
+	   	byte[] dataBytes = Encoding.UTF8.GetBytes(s);
+
+	   	string signature;
+	   	using (System.Security.Cryptography.HMACSHA256 hmac = new System.Security.Cryptography.HMACSHA256(keyBytes))
+	   	{
+	   		byte[] signatureBytes = hmac.ComputeHash(dataBytes);
+	   		signature = Convert.ToBase64String(signatureBytes);
+	   	}
+
+	   	object result2 = await InternetContent.PostAsync(
+	   		new Uri("https://" + host + "/Agent/Account/Login"),
+	    new Dictionary<string, object>()
+	   		{
+	   			{ "userName", ServiceRef.TagProfile.Account },
+	   			{ "nonce", nonce.ToString() },
+	   			{ "signature", signature },
+	   			{ "seconds", 3600 }
+	   		},
+	   		new KeyValuePair<string, string>("Accept", "application/json"),
+	   		new KeyValuePair<string, string>("Accept-Language", AcceptLanguage),
+	   		new KeyValuePair<string, string>("Accept-Encoding", "0")
+	   		);
+	   	string jwt = string.Empty;
+	   	if (result2 is Dictionary<string, object> Response)
+	   	{
+	   		if (Response.TryGetValue("jwt", out object? Obj) && Obj is string JWT)
+	   			jwt = JWT;
+	   	}
+	   	Console.WriteLine("JWT: " + jwt);
+	   	object result = await InternetContent.PostAsync(new Uri("https://" + host + "/Agent/Account/Recover"),
+	    new Dictionary<string, object>
+	   		{
+	   			{ "country", ""},
+	   			{ "eMail", ServiceRef.TagProfile.EMail },
+	   			{ "personalNr", ""},
+	   			{ "phoneNr", ""},
+	   			{ "userName", ServiceRef.TagProfile.Account }
+	   		},
+	   			new KeyValuePair<string, string>("Accept", "application/json"),
+	   			new KeyValuePair<string, string>("Accept-Language", AcceptLanguage),
+	   			new KeyValuePair<string, string>("Accept-Encoding", "0"),
+	   			new KeyValuePair<string, string>("Authorization", "Bearer " + jwt)
+	   	);
+
+	   }
+	   catch (Exception ex)
+	   {
+	   	Console.WriteLine(ex.Message);
+	   }
+	 */
 }
