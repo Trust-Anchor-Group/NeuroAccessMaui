8604ea4434cef46d686fc0343f56d479434c3952
berggrenmille <berggrenmille@hotmail.se>
2024-07-24 14:07:14 +0200

    Fixed Formatting

--- NeuroAccessMaui/Platforms/iOS/AppDelegate.cs
+++ NeuroAccessMaui/Platforms/iOS/AppDelegate.cs
@@ -14,8 +14,8 @@ namespace NeuroAccessMaui
 		protected override MauiApp CreateMauiApp()
 		{
 			MauiApp app = MauiProgram.CreateMauiApp();
-			#warning Remove this line when the issue with the RadioButton control template is fixed in a future version of MAUI https://github.com/dotnet/maui/issues/19478
-			
+#warning Remove this line when the issue with the RadioButton control template is fixed in a future version of MAUI https://github.com/dotnet/maui/issues/19478
+
 			// TODO: This is a temporary workaround to fix the issue with custom RadioButton control templates not responding to taps on IOS
 			// https://github.com/dotnet/maui/issues/19478
 			this.RadioButtonTemplateWorkaround();
@@ -43,9 +43,9 @@ namespace NeuroAccessMaui
 
 		private void EnsureKeyWindow()
 		{
-			if(GetKeyWindow() is null)
+			if (GetKeyWindow() is null)
 				return;
-				
+
 			UIWindow? window = null;
 			if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
 			{
@@ -56,10 +56,10 @@ namespace NeuroAccessMaui
 			}
 			else
 				window = UIApplication.SharedApplication.Windows.FirstOrDefault();
-			
+
 			if (window is null)
 				return;
-			if(!window!.IsKeyWindow)
+			if (!window!.IsKeyWindow)
 				window!.MakeKeyWindow();
 
 		}
@@ -70,18 +70,18 @@ namespace NeuroAccessMaui
 		/// </summary>
 		internal static UIWindow? GetKeyWindow()
 		{
-			if(UIDevice.CurrentDevice.CheckSystemVersion(15, 0))
+			if (UIDevice.CurrentDevice.CheckSystemVersion(15, 0))
 			{
 				return UIApplication.SharedApplication.ConnectedScenes
 					.OfType<UIWindowScene>()
 					.SelectMany(s => s.Windows)
 					.FirstOrDefault(w => w.IsKeyWindow);
 			}
-            else if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
-                return UIApplication.SharedApplication.Windows.FirstOrDefault(w => w.IsKeyWindow);
-            else
+			else if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+				return UIApplication.SharedApplication.Windows.FirstOrDefault(w => w.IsKeyWindow);
+			else
 				return UIApplication.SharedApplication.KeyWindow;
-		}	
+		}
 		private void RadioButtonTemplateWorkaround()
 		{
 			Microsoft.Maui.Handlers.RadioButtonHandler.Mapper.AppendToMapping("TemplateWorkaround", (h, v) =>
@@ -96,30 +96,30 @@ namespace NeuroAccessMaui
 			});
 		}
 
-/*
-Not needed anymore as we have a new way to handle keyboard events in PlatformSpecific.cs, keeping this until new implementation has been tested
-		private void RegisterKeyBoardObserver()
-		{
-			this.onKeyboardShowObserver ??= UIKeyboard.Notifications.ObserveWillShow((object? Sender, UIKeyboardEventArgs Args) =>
-			{
-				NSDictionary? UserInfo = Args.Notification.UserInfo;
-
-				if (UserInfo is null)
+		/*
+		Not needed anymore as we have a new way to handle keyboard events in PlatformSpecific.cs, keeping this until new implementation has been tested
+				private void RegisterKeyBoardObserver()
 				{
-					return;
+					this.onKeyboardShowObserver ??= UIKeyboard.Notifications.ObserveWillShow((object? Sender, UIKeyboardEventArgs Args) =>
+					{
+						NSDictionary? UserInfo = Args.Notification.UserInfo;
+
+						if (UserInfo is null)
+						{
+							return;
+						}
+
+						NSValue Result = (NSValue)UserInfo.ObjectForKey(new NSString(UIKeyboard.FrameEndUserInfoKey));
+						CGSize keyboardSize = Result.RectangleFValue.Size;
+
+						WeakReferenceMessenger.Default.Send(new KeyboardSizeMessage((float)keyboardSize.Height));
+					});
+
+					this.onKeyboardHideObserver ??= UIKeyboard.Notifications.ObserveWillHide((object? Sender, UIKeyboardEventArgs Args) =>
+					{
+						WeakReferenceMessenger.Default.Send(new KeyboardSizeMessage(0));
+					});
 				}
-
-				NSValue Result = (NSValue)UserInfo.ObjectForKey(new NSString(UIKeyboard.FrameEndUserInfoKey));
-				CGSize keyboardSize = Result.RectangleFValue.Size;
-
-				WeakReferenceMessenger.Default.Send(new KeyboardSizeMessage((float)keyboardSize.Height));
-			});
-
-			this.onKeyboardHideObserver ??= UIKeyboard.Notifications.ObserveWillHide((object? Sender, UIKeyboardEventArgs Args) =>
-			{
-				WeakReferenceMessenger.Default.Send(new KeyboardSizeMessage(0));
-			});
-		}
-*/
+		*/
 	}
 }
