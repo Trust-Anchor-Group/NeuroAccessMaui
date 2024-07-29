af028d51e670f84dcda89dbfd7f185d9106df2c7
Maximiliam Berggren <maximiliam.berggren@trustanchorgroup.com>
2024-07-29 17:24:21 +0200

    Added NewContractStep.cs

---
+++ NeuroAccessMaui/UI/Pages/Contracts/NewContract/NewContractStep.cs
@@ -0,0 +1,22 @@
+namespace NeuroAccessMaui.UI.Pages.Contracts.NewContract
+{
+    /// <summary>
+    /// The different steps of creating a new contract.
+    /// </summary>
+    public enum NewContractStep
+    {
+        Overview = 10,
+        Loading = 20,
+        Parameters = 30,
+        Parts = 40,
+
+        Preview = 50,
+
+        Final = 60
+        
+        
+
+
+        
+    }
+}
