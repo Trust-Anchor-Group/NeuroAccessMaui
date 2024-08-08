ed8a815842a8ba1bdfb830d522a43c79e107d0ef
Maximiliam Berggren <maximiliam.berggren@trustanchorgroup.com>
2024-08-08 16:44:45 +0200

    Added Factory method

--- NeuroAccessMaui/UI/Pages/Contracts/NewContract/ObjectModel/ParameterInfo2.cs
+++ NeuroAccessMaui/UI/Pages/Contracts/NewContract/ObjectModel/ParameterInfo2.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using NeuroAccessMaui.Extensions;
+using NeuroAccessMaui.Services;
 using Waher.Content;
 using Waher.Networking.XMPP.Contracts;
 
@@ -9,51 +10,98 @@ namespace NeuroAccessMaui.UI.Pages.Contracts.NewContract.ObjectModel
 	/// <summary>
 	/// An observable object that wraps a <see cref="Waher.Networking.XMPP.Contracts.Parameter"/> object.
 	/// This allows for easier binding in the UI.
+	/// Must be either Initialized with <see cref="CreateAsync"/> or <see cref="InitializeAsync"/>
 	/// </summary>
 	public class ParameterInfo2 : ObservableObject
 	{
-		public ParameterInfo2(Parameter parameter)
+		protected ParameterInfo2(Parameter parameter)
 		{
 			this.Parameter = parameter;
 		}
 
 		/// <summary>
 		/// Initializes the parameter in regards to a contract.
-		/// E.g Sets the description of the parameter, with the contract language.
 		/// </summary>
-		/// <param name="contract"></param>
-		/// <returns></returns>
-		public async Task InitializeWithContractAsync(Contract contract)
+		public async Task InitializeAsync(Contract contract)
 		{
-			this.Description = await contract.ToPlainText(this.Parameter.Descriptions, contract.DeviceLanguage());
+			try
+			{
+				this.Description = await contract.ToPlainText(this.Parameter.Descriptions, contract.DeviceLanguage());
+			}
+			catch (Exception e)
+			{
+				ServiceRef.LogService.LogException(e);
+			}
+		}
+
+		/// <summary>
+		///  Creates a new instance of <see cref="ParameterInfo2"/> based on the type of the parameter.
+		/// </summary>
+		/// <param name="parameter">The parameter to wrap</param>
+		/// <param name="contract">The contract that</param>
+		/// <returns></returns>
+		public static async Task<ParameterInfo2> CreateAsync(Parameter parameter, Contract contract)
+		{
+			
+			ParameterInfo2 parameterInfo = parameter switch
+			{
+				BooleanParameter booleanParameter => new BooleanParameterInfo(booleanParameter),
+				DateParameter dateParameter => new DateParameterInfo(dateParameter),
+				NumericalParameter numericalParameter => new NumericalParameterInfo(numericalParameter),
+				StringParameter stringParameter => new StringParameterInfo(stringParameter),
+				TimeParameter timeParameter => new TimeParameterInfo(timeParameter),
+				DurationParameter durationParameter => new DurationParameterInfo(durationParameter),
+				_ => new ParameterInfo2(parameter)
+
+			};
+			await parameterInfo.InitializeAsync(contract);
+			return parameterInfo;
 		}
 
 		/// <summary>
 		/// The wrapped parameter object
 		/// </summary>
-		public Parameter Parameter { get; }
+		public Parameter Parameter { get; private set; }
+
 		/// <summary>
 		/// The name of the parameter
 		/// </summary>
 		public string Name => this.Parameter.Name;
 
 		/// <summary>
-		/// The label of the parameter
+		/// The Guide of the parameter
 		/// </summary>
 		public string Guide => string.IsNullOrEmpty(this.Parameter.Guide) ? this.Name : this.Parameter.Guide;
 
-		private string description = string.Empty;
+		public string Label { 
+			get {
+				if (string.IsNullOrEmpty(this.Description))
+				{
+					if(string.IsNullOrEmpty(this.Guide))
+					{
+						return this.Name;
+					}
+					return this.Guide;
+				}
+				return this.Description;
+			}
+		}
+
 		/// <summary>
 		/// The localized description of the parameter
-		/// Has to be initialized with <see cref="InitializeWithContractAsync"/>
 		/// </summary>
 		public string Description
 		{
 			get => this.description;
-			private set => this.SetProperty(ref this.description, value);
-		}
+			private set
+			{
+				if(this.SetProperty(ref this.description, value))
+					this.OnPropertyChanged(nameof(this.Label));
 
-		private bool error;
+			}
+		}
+		private string description = string.Empty;
+
 		/// <summary>
 		/// If the parameter has an validation error
 		/// </summary>
@@ -62,6 +110,7 @@ namespace NeuroAccessMaui.UI.Pages.Contracts.NewContract.ObjectModel
 			get => this.error;
 			set => this.SetProperty(ref this.error, value);
 		}
+		private bool error;
 
 		private string errorText = string.Empty;
 
@@ -80,63 +129,50 @@ namespace NeuroAccessMaui.UI.Pages.Contracts.NewContract.ObjectModel
 		}
 
 
-		public virtual object Value { get; set; }
+		private object? value;
+		public object? Value 
+		{ 
+			get => this.value; 
+			set			
+			{
+					this.SetProperty(ref this.value, value);
+					try
+					{
+						if(value is not null)
+							this.Parameter.SetValue(value);
+					}
+					catch (Exception e)
+					{
+						ServiceRef.LogService.LogException(e);
+					}
+			}
+		}
 	}
 
-	public class BooleanParameterInfo : ParameterInfo2
+	public sealed class BooleanParameterInfo : ParameterInfo2
 	{
-		private bool value;
 
 		public BooleanParameterInfo(BooleanParameter parameter) : base(parameter)
 		{
-			this.value = parameter.ObjectValue is true;
+			this.Value = parameter.ObjectValue is true;
 		}
-
-		public override object Value
-		{
-			get => this.value;
-			set
-			{
-				if (value is bool boolValue)
-				{
-					this.Parameter.SetValue(boolValue);
-					this.SetProperty(ref this.value, boolValue);
-				}
-			}
-		}
-
 		public bool BooleanValue
 		{
-			get => this.value;
+			get => this.Value as bool? ?? false;
 			set => this.Value = value;
 		}
 	}
 
 	public class DateParameterInfo : ParameterInfo2
 	{
-		private DateTime? value;
-
 		public DateParameterInfo(DateParameter parameter) : base(parameter)
 		{
-			this.value = parameter.ObjectValue as DateTime?;
-		}
-
-		public override object? Value
-		{
-			get => this.value;
-			set
-			{
-				if (value is DateTime dateTime)
-				{
-					this.Parameter.SetValue(dateTime);
-					this.SetProperty(ref this.value, dateTime);
-				}
-			}
+			this.Value = parameter.ObjectValue as DateTime?;
 		}
 
 		public DateTime? DateValue
 		{
-			get => this.value;
+			get => this.Value as DateTime?;
 			set => this.Value = value;
 		}
 		
@@ -144,116 +180,60 @@ namespace NeuroAccessMaui.UI.Pages.Contracts.NewContract.ObjectModel
 
 	public class NumericalParameterInfo : ParameterInfo2
 	{
-		private decimal value;
-
 		public NumericalParameterInfo(NumericalParameter parameter) : base(parameter)
 		{
-			this.value = parameter.ObjectValue is decimal decimalValue ? decimalValue : 0;
+			this.Value = parameter.ObjectValue is decimal decimalValue ? decimalValue : 0;
 		}
 
-		public override object Value
-		{
-			get => this.value;
-			set
-			{
-				if (value is decimal decimalValue)
-				{
-					this.Parameter.SetValue(decimalValue);
-					this.SetProperty(ref this.value, decimalValue);
-				}
-			}
-		}
 
 		public decimal DecimalValue
 		{
-			get => this.value;
+			get => this.Value as decimal? ?? 0;
 			set => this.Value = value;
 		}
 	}
 
 	public class StringParameterInfo : ParameterInfo2
 	{
-		private string value;
 
 		public StringParameterInfo(StringParameter parameter) : base(parameter)
 		{
-			this.value = parameter.ObjectValue as string ?? string.Empty;
-		}
-
-		public override object Value
-		{
-			get => this.value;
-			set
-			{
-				if (value is string stringValue)
-				{
-					this.Parameter.SetValue(stringValue);
-					this.SetProperty(ref this.value, stringValue);
-				}
-			}
+			this.Value = parameter.ObjectValue as string ?? string.Empty;
 		}
 
 		public string StringValue
 		{
-			get => this.value;
+			get => this.Value as string ?? string.Empty;
 			set => this.Value = value;
 		}
 	}
 
 	public class TimeParameterInfo : ParameterInfo2
 	{
-		private TimeSpan value;
-
 		public TimeParameterInfo(TimeParameter parameter) : base(parameter)
 		{
-			this.value = parameter.ObjectValue is TimeSpan timeSpan ? timeSpan : TimeSpan.Zero;
+			this.Value = parameter.ObjectValue is TimeSpan timeSpan ? timeSpan : TimeSpan.Zero;
 		}
 
-		public override object Value
-		{
-			get => this.value;
-			set
-			{
-				if (value is TimeSpan timeSpan)
-				{
-					this.Parameter.SetValue(timeSpan);
-					this.SetProperty(ref this.value, timeSpan);
-				}
-			}
-		}
+
 
 		public TimeSpan TimeSpanValue
 		{
-			get => this.value;
+			get => this.Value as TimeSpan? ?? TimeSpan.Zero;
 			set => this.Value = value;
 		}
 	}
 
 	public class DurationParameterInfo : ParameterInfo2
 	{
-		private Duration value;
-
 		public DurationParameterInfo(DurationParameter parameter) : base(parameter)
 		{
-			this.value = parameter.ObjectValue is Duration duration ? duration : Duration.Zero;
+			this.Value = parameter.ObjectValue is Duration duration ? duration : Duration.Zero;
 		}
 
-		public override object Value
+		public Duration DurationValue
 		{
-			get => this.value;
-			set
-			{
-				if (value is Duration duration)
-				{
-					this.Parameter.SetValue(duration);
-					this.SetProperty(ref this.value, duration);
-				}
-			}
-		}
-
-		public Duration DurationParameterValue
-		{
-			get => this.value;
+			get => this.Value as Duration? ?? Duration.Zero;
 			set => this.Value = value;
 		}
 	}
