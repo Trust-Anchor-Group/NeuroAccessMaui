eb4753a7efe9f2ee8619be08852a8bfb2e41f89f
berggrenmille <berggrenmille@hotmail.se>
2024-09-22 23:57:39 +0200

    Add NameOption enum and necessary using directives
    Introduced a new `NameOption` enumeration in the `NeuroAccessMaui.UI.Pages.Registration` namespace with options: `RealName`, `Nickname`, and `Anonymous`. Added `using` directives for `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks` to ensure proper functionality.

---
+++ NeuroAccessMaui/UI/Pages/Registration/NameOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroAccessMaui.UI.Pages.Registration
+{
+	public enum NameOption
+	{
+		RealName,
+		Nickname,
+		Anonymous
+	}
+}
