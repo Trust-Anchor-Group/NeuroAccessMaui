0411d398ce12cff6578cd7d01d95d6cb0a4a0b2b
Maximiliam Berggren <maximiliam.berggren@trustanchorgroup.com>
2024-09-24 16:00:41 +0200

    Add Account Details to ContactSupportVIew

--- NeuroAccessMaui/UI/Pages/Registration/Views/FinalizeViewModel.cs
+++ NeuroAccessMaui/UI/Pages/Registration/Views/FinalizeViewModel.cs
@@ -1,5 +1,12 @@
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using NeuroAccessMaui.Resources.Languages;
+using NeuroAccessMaui.Services;
+using NeuroAccessMaui.Services.Localization;
 using NeuroAccessMaui.Services.Tag;
+using NeuroAccessMaui.UI.Popups.Info;
+using Waher.Content;
 
 namespace NeuroAccessMaui.UI.Pages.Registration.Views
 {
@@ -9,6 +16,66 @@ namespace NeuroAccessMaui.UI.Pages.Registration.Views
 			: base(RegistrationStep.Finalize)
 		{
 		}
+
+		protected override async Task OnInitialize()
+		{
+			await base.OnInitialize();
+
+			LocalizationManager.Current.PropertyChanged += this.Localization_Changed;
+		}
+
+		/// <inheritdoc />
+		protected override async Task OnDispose()
+		{
+			LocalizationManager.Current.PropertyChanged -= this.Localization_Changed;
+
+			await base.OnDispose();
+		}
+
+
+		public override async Task DoAssignProperties()
+		{
+			await base.DoAssignProperties();
+			await this.SetDomainName();
+			this.NetworkID = $"{ServiceRef.TagProfile.Account}@{ServiceRef.TagProfile.Domain}";
+		}
+
+		private async void Localization_Changed(object? Sender, PropertyChangedEventArgs e)
+		{
+			await this.SetDomainName();
+		}
+
+		private async Task SetDomainName()
+		{
+			this.DomainName = ServiceRef.TagProfile.Domain!;
+
+			try
+			{
+				Uri DomainInfo = new("https://" + this.DomainName + "/Agent/Account/DomainInfo");
+				string AcceptLanguage = App.SelectedLanguage.TwoLetterISOLanguageName;
+
+				if (AcceptLanguage != "en")
+					AcceptLanguage += ";q=1,en;q=0.9";
+
+				object Result = await InternetContent.GetAsync(DomainInfo,
+					new KeyValuePair<string, string>("Accept", "application/json"),
+					new KeyValuePair<string, string>("Accept-Language", AcceptLanguage),
+					new KeyValuePair<string, string>("Accept-Encoding", "0"));
+				if (Result is Dictionary<string, object> Response)
+				{
+					if (Response.TryGetValue("humanReadableName", out object? Obj) && Obj is string LocalizedName)
+						this.LocalizedDomainName = LocalizedName;
+
+					if (Response.TryGetValue("humanReadableDescription", out Obj) && Obj is string LocalizedDescription)
+						this.LocalizedDomainDescription = LocalizedDescription;
+				}
+			}
+			catch (Exception ex)
+			{
+				ServiceRef.LogService.LogException(ex);
+			}
+		}
+
 
 		/// <summary>
 		/// Gets the size of the background for the checkmark.
@@ -24,10 +91,64 @@ namespace NeuroAccessMaui.UI.Pages.Registration.Views
 		/// </summary>
 		public double CheckmarkIconSize => 60.0;
 
+		/// <summary>
+		/// The network ID of the account.
+		/// </summary>
+		[ObservableProperty]
+		private string? networkID;
+
         [RelayCommand]
 		private void Continue()
 		{
 			GoToRegistrationStep(RegistrationStep.Complete);
+		}
+
+		/// <summary>
+		/// Copies Item to clipboard
+		/// </summary>
+		[RelayCommand]
+		private async Task Copy(object Item)
+		{
+			try
+			{
+				this.SetIsBusy(true);
+
+				if (Item is string Label)
+				{
+					await Clipboard.SetTextAsync(Label);
+					await ServiceRef.UiService.DisplayAlert(
+						ServiceRef.Localizer[nameof(AppResources.SuccessTitle)],
+						ServiceRef.Localizer[nameof(AppResources.TagValueCopiedToClipboard)]);
+				}
+			}
+			catch (Exception ex)
+			{
+				ServiceRef.LogService.LogException(ex);
+				await ServiceRef.UiService.DisplayException(ex);
+			}
+			finally
+			{
+				this.SetIsBusy(false);
+			}
+		}
+
+
+		[ObservableProperty]
+		private string domainName = string.Empty;
+
+		[ObservableProperty]
+		private string localizedDomainName = string.Empty;
+
+		[ObservableProperty]
+		private string localizedDomainDescription = string.Empty;
+
+		[RelayCommand]
+		private async Task ServiceProviderInfo()
+		{
+			string title = this.LocalizedDomainName;
+			string message = this.LocalizedDomainDescription;
+			ShowInfoPopup infoPage = new(title, message);
+			await ServiceRef.UiService.PushAsync(infoPage);
 		}
     }
 }
