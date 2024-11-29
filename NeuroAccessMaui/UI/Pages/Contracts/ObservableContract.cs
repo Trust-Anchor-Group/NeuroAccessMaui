2fed90e66a6fdc7e8730ac92cbbb3cbcd4244e03
berggrenmille <berggrenmille@hotmail.se>
2024-11-29 01:12:28 +0100

    Moved removed files

---
+++ NeuroAccessMaui/UI/Pages/Contracts/ObservableContract.cs
@@ -0,0 +1,205 @@
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using NeuroAccessMaui.UI.Pages.Contracts.MyContracts.ObjectModels;
+using Waher.Networking.XMPP.Contracts;
+
+namespace NeuroAccessMaui.UI.Pages.Contracts.ObjectModel
+{
+	/// <summary>
+	/// An observable object that wraps a <see cref="Contract"/> object.
+	/// This allows for easier binding in the UI.
+	/// Either create instances with <see cref="CreateAsync"/> or initialize with <see cref="InitializeAsync"/>.
+	/// </summary>
+	public class ObservableContract : ObservableObject
+	{
+
+		#region Constructors and Destructor
+
+		private ObservableContract(Contract contract)
+		{
+			this.Contract = contract;
+			this.Parameters.CollectionChanged += this.Parameters_CollectionChanged;
+			this.Roles.CollectionChanged += this.Roles_CollectionChanged;
+		}
+
+		#endregion
+
+		#region Initialization
+
+		/// <summary>
+		/// Creates a new instance of <see cref="ObservableContract"/> and initializes the roles and parameters.
+		/// </summary>
+		/// <param name="contract">The Contract object to wrap.</param>
+		public static async Task<ObservableContract> CreateAsync(Contract contract)
+		{
+			ObservableContract contractWrapper = new(contract);
+			await contractWrapper.InitializeAsync();
+			return contractWrapper;
+		}
+
+		/// <summary>
+		/// Initializes the contract data, such as category and parameters.
+		/// </summary>
+		private async Task InitializeAsync()
+		{
+			this.Category = await ContractModel.GetCategory(this.Contract) ?? string.Empty;
+
+			foreach (Parameter param in this.Contract.Parameters ?? Enumerable.Empty<Parameter>())
+			{
+				ObservableParameter observableParam = await ObservableParameter.CreateAsync(param, this.Contract);
+				this.Parameters.Add(observableParam);
+			}
+
+			foreach (Role role in this.Contract.Roles ?? Enumerable.Empty<Role>())
+			{
+				ObservableRole observableRole = new(role);
+				await observableRole.InitializeAsync(this.Contract);
+				this.Roles.Add(observableRole);
+			}
+
+			foreach (Part part in this.Contract.Parts ?? Enumerable.Empty<Part>())
+			{
+				ObservableRole? Role = this.Roles.FirstOrDefault(r => r.Name == part.Role);
+				if (Role is not null)
+					await Role.AddPart(part);
+			}
+		}
+
+		#endregion
+
+		#region Collection Changed Handlers
+
+		private void Parameters_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+		{
+			if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems != null)
+			{
+				foreach (ObservableParameter param in e.NewItems)
+				{
+					param.PropertyChanged += this.Parameter_OnPropertyChanged;
+				}
+			}
+			else if (e.Action == NotifyCollectionChangedAction.Remove && e.OldItems != null)
+			{
+				foreach (ObservableParameter param in e.OldItems)
+				{
+					param.PropertyChanged -= this.Parameter_OnPropertyChanged;
+				}
+			}
+		}
+
+		private void Roles_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+		{
+			if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems != null)
+			{
+				foreach (ObservableRole role in e.NewItems)
+				{
+					role.PropertyChanged += this.Role_OnPropertyChanged;
+					role.Parts.CollectionChanged += this.Parts_CollectionChanged;
+				}
+			}
+			else if (e.Action == NotifyCollectionChangedAction.Remove && e.OldItems != null)
+			{
+				foreach (ObservableRole role in e.OldItems)
+				{
+					role.PropertyChanged -= this.Role_OnPropertyChanged;
+					role.Parts.CollectionChanged -= this.Parts_CollectionChanged;
+				}
+			}
+		}
+
+		#endregion
+
+		#region Property Changed Handlers
+
+		public event NotifyCollectionChangedEventHandler? PartChanged;
+		private void Parts_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+		{
+			this.PartChanged?.Invoke(sender, e);
+		}
+
+		/// <summary>
+		/// Occurs when a property value of any parameter changes.
+		/// </summary>
+		public event PropertyChangedEventHandler? ParameterChanged;
+
+		private void Parameter_OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			this.ParameterChanged?.Invoke(sender, e);
+		}
+
+		/// <summary>
+		/// Occurs when a property value of any role changes.
+		/// </summary>
+		public event PropertyChangedEventHandler? RoleChanged;
+
+		private void Role_OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			this.RoleChanged?.Invoke(sender, e);
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// The wrapped contract object.
+		/// </summary>
+		public Contract Contract { get; }
+
+		/// <summary>
+		/// The parameters of the contract.
+		/// </summary>
+		public ObservableCollection<ObservableParameter> Parameters => this.parameters;
+		private readonly ObservableCollection<ObservableParameter> parameters = [];
+
+		/// <summary>
+		/// The roles of the contract.
+		/// </summary>
+		public ObservableCollection<ObservableRole> Roles => this.roles;
+		private readonly ObservableCollection<ObservableRole> roles = [];
+
+		/// <summary>
+		/// The category of the contract.
+		/// </summary>
+		public string Category
+		{
+			get => this.category;
+			private set => this.SetProperty(ref this.category, value);
+		}
+		private string category = string.Empty;
+
+		/// <summary>
+		/// The contract state.
+		/// </summary>
+		public ContractState ContractState => this.Contract.State;
+
+		/// <summary>
+		/// Whether the contract can act as a template.
+		/// </summary>
+		public bool CanActAsTemplate => this.Contract.CanActAsTemplate;
+
+		/// <summary>
+		/// Whether the contract is a template.
+		/// </summary>
+		public bool IsTemplate => this.Contract.PartsMode == ContractParts.TemplateOnly;
+
+		/// <summary>
+		/// The visibility of the contract.
+		/// </summary>
+		public ContractVisibility Visibility => this.Contract.Visibility;
+
+		/// <summary>
+		/// The contract ID.
+		/// </summary>
+		public string ContractId => this.Contract.ContractId;
+
+		/// <summary>
+		/// The template ID.
+		/// </summary>
+		public string TemplateId => this.Contract.TemplateId;
+
+		#endregion
+	}
+}
---
+++ NeuroAccessMaui/UI/Pages/Contracts/ObservablePart.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NeuroAccessMaui.Extensions;
+using NeuroAccessMaui.Resources.Languages;
+using NeuroAccessMaui.Services;
+using NeuroAccessMaui.Services.Contacts;
+using NeuroAccessMaui.Services.Notification;
+using NeuroAccessMaui.Services.Notification.Identities;
+using NeuroAccessMaui.UI.Pages.Identity.ViewIdentity;
+using Waher.Networking.XMPP.Contracts;
+using Waher.Persistence;
+using Waher.Persistence.Filters;
+
+namespace NeuroAccessMaui.UI.Pages.Contracts.ObjectModel
+{
+	public partial class ObservablePart : ObservableObject, IDisposable
+	{
+		public ObservablePart(Part part)
+		{
+			this.Part = part;
+			ServiceRef.XmppService.PetitionedIdentityResponseReceived += this.XmppService_OnPetitionedIdentityResponseReceived;
+		}
+		/// <summary>
+		/// Initializes the part, setting properties which needs to be set asynchronosly
+		/// </summary>
+		/// <param name="contract"></param>
+		public async Task InitializeAsync()
+		{
+			try
+			{
+				// Set Friendly name before anything can go wrong
+				this.FriendlyName = await this.GetFriendlyNameAsync();
+
+
+				this.identity = await ServiceRef.ContractOrchestratorService.TryGetLegalIdentity(this.LegalId,
+									ServiceRef.Localizer[nameof(AppResources.ForInclusionInContract)]);
+
+
+				MainThread.BeginInvokeOnMainThread(() =>
+				{
+					this.OnPropertyChanged(nameof(this.HasIdentity));
+				});
+
+				// Update the friendly name after the identity might be set
+				this.FriendlyName = await this.GetFriendlyNameAsync();
+			}
+			catch (Exception e)
+			{
+				ServiceRef.LogService.LogException(e);
+			}
+		}
+
+		private async Task XmppService_OnPetitionedIdentityResponseReceived(object? Sender, LegalIdentityPetitionResponseEventArgs e)
+		{
+			Console.WriteLine(e.Response + " : " + e.RequestedIdentity is not null);
+			if (e.RequestedIdentity is not null && e.RequestedIdentity.Id == this.LegalId)
+			{
+				this.identity = e.RequestedIdentity;
+				string FriendlyName = await this.GetFriendlyNameAsync();
+
+				MainThread.BeginInvokeOnMainThread(() =>
+				{
+					this.FriendlyName = FriendlyName;
+					this.OnPropertyChanged(nameof(this.HasIdentity));
+				});
+			}
+		}
+
+		private async Task<string> GetFriendlyNameAsync()
+		{
+			try
+			{
+				if (this.IsMe)
+					return ServiceRef.Localizer[nameof(AppResources.Me)];
+
+				if (this.Part.LegalId == ServiceRef.TagProfile.TrustProviderId && !string.IsNullOrEmpty(ServiceRef.TagProfile.Domain))
+					return ServiceRef.TagProfile.Domain;
+
+				ContactInfo info = await Database.FindFirstIgnoreRest<ContactInfo>(new FilterFieldEqualTo("LegalId", this.Part.LegalId));
+				if (info is not null && !string.IsNullOrEmpty(info.FriendlyName))
+					return info.FriendlyName;
+
+				if (this.identity is not null)
+					return ContactInfo.GetFriendlyName(this.identity);
+
+				if (info is not null && !string.IsNullOrEmpty(info.BareJid))
+					return info.BareJid;
+			}
+			catch (Exception e)
+			{
+				//Ignore
+			}
+
+			return this.LegalId;
+		}
+		#region Properties
+		private LegalIdentity? identity;
+
+		/// <summary>
+		/// The wrapped Part object
+		/// </summary>
+		public Part Part { get; }
+
+		public string LegalId => this.Part.LegalId;
+
+
+		public string Role => this.Part.Role;
+
+		/// <summary>
+		/// The friendly name for the part
+		/// Has to be initialized with <see cref="InitializeAsync"/>
+		/// </summary>
+		public string? FriendlyName
+		{
+			get => this.friendlyName;
+			private set
+			{
+				this.SetProperty(ref this.friendlyName, value);
+			}
+		}
+		private string? friendlyName = string.Empty;
+
+		public bool IsMe => this.Part.LegalId == ServiceRef.TagProfile.LegalIdentity?.Id;
+		public bool IsThirdParty => !this.IsMe;
+
+		public bool HasIdentity => this.identity is not null;
+
+		public bool HasSigned
+		{
+			get => this.hasSigned;
+			private set
+			{
+				this.SetProperty(ref this.hasSigned, value);
+			}
+		}
+		private bool hasSigned = false;
+
+		#endregion
+
+
+		#region Commands
+		[RelayCommand(AllowConcurrentExecutions = false)]
+		private async Task OpenIdentityAsync()
+		{
+			if (this.identity is null)
+			{
+				await ServiceRef.UiService.DisplayAlert(ServiceRef.Localizer[nameof(AppResources.Petition)], ServiceRef.Localizer[nameof(AppResources.PetitionIdentitySent)], ServiceRef.Localizer[nameof(AppResources.Ok)]);
+				await this.InitializeAsync();
+			}
+			else
+			{
+				await ServiceRef.UiService.GoToAsync(nameof(ViewIdentityPage), new ViewIdentityNavigationArgs(this.identity));
+			}
+		}
+		#endregion
+
+		private bool disposed = false;
+
+		#region IDisposable Support
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (this.disposed)
+				return;
+
+			if (disposing)
+			{
+				// Unsubscribe from the event to prevent memory leaks
+				ServiceRef.XmppService.PetitionedIdentityResponseReceived -= this.XmppService_OnPetitionedIdentityResponseReceived;
+			}
+
+			this.disposed = true;
+		}
+		#endregion
+	}
+}
