77b2d9e17a7a8899d3d3c3bde7ca0cae513073fe
luma <lucas.martinahlm@gmail.com>
2025-01-30 11:46:19 +0100

    Refactor UI tests with BaseTest class for Appium setup
    Introduce BaseTest class in NeuroAccess.UiTests for common UI test setup using Appium and NUnit.
    - Add AppiumDriver field and App property in BaseTest.
    - Implement [SetUp], [OneTimeSetUp], [TearDown], and [OneTimeTearDown] methods in BaseTest.
    - Update UnitTest1 to inherit from BaseTest, removing redundant setup.
    - Enhance Test1 method to take a screenshot and print a platform-specific message.
    - Use BaseTest's App property in Test1 for driver interactions.

---
+++ NeuroAccess.Tests/BaseTest.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace NeuroAccess.UiTests
+{
+	
+	public class BaseTest
+	{
+		private static AppiumDriver? driver;
+
+		public static AppiumDriver App => driver ?? throw new InvalidOperationException("AppiumDriver is not initialized.");
+
+
+		// This will run before each test in the derived classes
+		[SetUp]
+		public void SetUp()
+		{
+			Console.WriteLine("Setting up for test...");
+			// Initialize any necessary objects, mock services, or test-specific setup here
+		}
+
+		// This will run once before all tests in the derived classes
+		[OneTimeSetUp]
+		public void RunBeforeAnyTests()
+		{
+			// If you started an Appium server manually, make sure to comment out the next line
+			// This line starts a local Appium server for you as part of the test run
+			AppiumServerHelper.StartAppiumLocalServer();
+
+			var androidOptions = new AppiumOptions
+			{
+				// Specify UIAutomator2 as the driver, typically don't need to change this
+				AutomationName = "UIAutomator2",
+				// Always Android for Android
+				PlatformName = "Android",
+				// This is the Android version, not API level
+				// This is ignored if you use the avd option below
+				PlatformVersion = "14",
+				// The full path to the .apk file to test or the package name if the app is already installed on the device
+				App = "com.tag.NeuroAccess.apk",
+			};
+			driver = new AndroidDriver(androidOptions);
+		}
+
+		// This will run after each test in the derived classes
+		[TearDown]
+		public void TearDown()
+		{
+			Console.WriteLine("Tearing down after test...");
+			// Perform any cleanup for each individual test here
+			// For example, resetting states, clearing mock data, etc.
+		}
+
+		// This will run once after all tests in the derived classes
+		[OneTimeTearDown]
+		public void RunAfterAnyTests()
+		{
+			driver?.Quit();
+			driver?.Dispose();
+
+			// If an Appium server was started locally above, make sure we clean it up here
+			AppiumServerHelper.DisposeAppiumLocalServer();
+		}
+	}
+}
--- NeuroAccess.Tests/UnitTest1.cs
+++ NeuroAccess.Tests/UnitTest1.cs
@@ -2,19 +2,14 @@ using NUnit.Framework;
 
 namespace NeuroAccess.UiTests
 {
-	public class Tests
+	public class UnitTest1 : BaseTest
 	{
-		[SetUp]
-		public void Setup()
-		{
-
-		}
-
 		[Test]
 		public void Test1()
 		{
+			App.GetScreenshot().SaveAsFile($"{nameof(Test1)}.png");
 #if ANDROID
-            Console.WriteLine("Running tests for Android.");
+			Console.WriteLine("Running tests for Android.");
 #elif IOS
             Console.WriteLine("Running tests for iOS.");
 #else
