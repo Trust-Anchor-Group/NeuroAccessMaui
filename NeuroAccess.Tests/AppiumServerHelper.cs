1d2c36e9735099ff2b8d3bd086cbba04bfde7ba3
luma <lucas.martinahlm@gmail.com>
2025-01-30 11:45:17 +0100

    Add Appium server management in NeuroAccess.UiTests
    Introduced a new namespace `NeuroAccess.UiTests` with a static
    class `AppiumServerHelper` to manage an Appium local server.
    Added methods to start and dispose of the server, using default
    or specified host and port values. Included constants for default
    host address and port, and a private static nullable field to
    track the server instance.

---
+++ NeuroAccess.Tests/AppiumServerHelper.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium.Appium.Service;
+
+namespace NeuroAccess.UiTests
+{
+
+	public static class AppiumServerHelper
+	{
+		private static AppiumLocalService? appiumLocalService;
+
+		public const string DefaultHostAddress = "127.0.0.1";
+		public const int DefaultHostPort = 4724;
+
+		public static void StartAppiumLocalServer(string host = DefaultHostAddress,
+			int port = DefaultHostPort)
+		{
+			if (appiumLocalService is not null)
+			{
+				return;
+			}
+
+			var builder = new AppiumServiceBuilder()
+				.WithIPAddress(host)
+				.UsingPort(port);
+
+			// Start the server with the builder
+			appiumLocalService = builder.Build();
+			appiumLocalService.Start();
+		}
+
+		public static void DisposeAppiumLocalServer()
+		{
+		
+			appiumLocalService?.Dispose();
+		}
+	}
+}
