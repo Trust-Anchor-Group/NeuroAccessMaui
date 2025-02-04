0634fd8885372ac16528894e6de6ec5d5701f91b
berggrenmille <berggrenmille@hotmail.se>
2025-02-04 13:54:59 +0100

    Fix missing file ending

--- NeuroAccess.Tests/Shared/BaseTest.cs
+++ NeuroAccess.Tests/Shared/BaseTest.cs
@@ -29,7 +29,7 @@ namespace NeuroAccess.UiTests
 				// This is ignored if you use the avd option below
 				PlatformVersion = "14",
 				// The full path to the .apk file to test or the package name if the app is already installed on the device
-				App = "com.tag.NeuroAccess",
+				App = "com.tag.NeuroAccess.apk",
 			};
 			driver = new AndroidDriver(androidOptions);
 		
