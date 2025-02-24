dc4c91a6641df263676912d924805000f43205ec
berggrenmille <berggrenmille@hotmail.se>
2025-02-24 12:40:47 +0100

    Add Error handling & platform specific text

--- NeuroAccessMaui/Services/AppPermissions/PermissionService.cs
+++ NeuroAccessMaui/Services/AppPermissions/PermissionService.cs
@@ -21,17 +21,23 @@ namespace NeuroAccessMaui.Services.AppPermissions
 		private static async Task<PermissionStatus> CheckAndRequestPermissionAsync<TPermission>()
 			 where TPermission : Permissions.BasePermission, new()
 		{
-			PermissionStatus Status = await Permissions.CheckStatusAsync<TPermission>();
-
-			// On platforms other than iOS, a Denied status might be temporary,
-			// so we try to request it.
-			if ((Status == PermissionStatus.Denied && DeviceInfo.Platform != DevicePlatform.iOS) ||
-				 Status == PermissionStatus.Unknown)
+			try
 			{
-				Status = await Permissions.RequestAsync<TPermission>();
-			}
+				PermissionStatus Status = await Permissions.CheckStatusAsync<TPermission>();
 
-			return Status;
+				// On platforms other than iOS, a Denied status might be temporary,
+				// so we try to request it.
+				if ((Status == PermissionStatus.Denied && DeviceInfo.Platform != DevicePlatform.iOS) ||
+					 Status == PermissionStatus.Unknown)
+				{
+					Status = await Permissions.RequestAsync<TPermission>();
+				}
+				return Status;
+			}
+			catch (Exception Ex)
+			{
+				return PermissionStatus.Unknown;
+			}
 		}
 
 		/// <summary>
@@ -45,17 +51,40 @@ namespace NeuroAccessMaui.Services.AppPermissions
 			if (Status == PermissionStatus.Granted)
 				return true;
 
+			string Title;
+			string Description;
+			string DescriptionSecondary;
+			Geometry IconGeometry;
 			// Prepare localized strings for the permission rationale
-			string Title = ServiceRef.Localizer[nameof(AppResources.CameraPermissionTitle)];
-			string Description = ServiceRef.Localizer[nameof(AppResources.CameraPermissionDescription)];
-			string DescriptionSecondary = ServiceRef.Localizer[nameof(AppResources.CameraPermissionDescriptionSecondary)];
-			Geometry IconGeometry = Geometries.CameraPhotoButtonPath;
+			if (OperatingSystem.IsAndroid())
+			{
+				Title = ServiceRef.Localizer[nameof(AppResources.CameraPermissionTitle)];
+				Description = ServiceRef.Localizer[nameof(AppResources.CameraPermissionDescription)];
+				DescriptionSecondary =
+					ServiceRef.Localizer[nameof(AppResources.CameraPermissionDescriptionSecondary)];
+				IconGeometry = Geometries.CameraPhotoButtonPath;
+			}
+			else if(OperatingSystem.IsIOS())
+			{
+				Title = ServiceRef.Localizer[nameof(AppResources.CameraPermissionTitle)];
+				Description = ServiceRef.Localizer[nameof(AppResources.CameraPermissionDescription)];
+				DescriptionSecondary =
+					ServiceRef.Localizer[nameof(AppResources.iOS_CameraPermissionDescriptionSecondary)];
+				IconGeometry = Geometries.CameraPhotoButtonPath;
+			}
+			else
+			{
+				Title = ServiceRef.Localizer[nameof(AppResources.CameraPermissionTitle)];
+				Description = ServiceRef.Localizer[nameof(AppResources.CameraPermissionDescription)];
+				DescriptionSecondary =
+					ServiceRef.Localizer[nameof(AppResources.CameraPermissionDescriptionSecondary)];
+				IconGeometry = Geometries.CameraPhotoButtonPath;
+			}
+
 			// Display the permission popup so the user can enable it from the settings.
 			ShowPermissionViewModel ViewModel = new(Title,Description,DescriptionSecondary, IconGeometry);
 			ShowPermissionPopup PermissionPopup = new(ViewModel);
 			await ServiceRef.UiService.PushAsync(PermissionPopup);
-
-			await ViewModel.Result;
 
 			Status = await CheckAndRequestPermissionAsync<Permissions.Camera>();
 
