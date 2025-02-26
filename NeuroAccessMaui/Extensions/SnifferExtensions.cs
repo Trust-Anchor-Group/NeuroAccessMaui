1ce47ad294e5aedf3fa91487c94a8f4f1cd128d2
Peter Waher <peterwaher@hotmail.com>
2025-02-26 16:38:08 -0300

    Fix: Capitalization

--- NeuroAccessMaui/Extensions/SnifferExtensions.cs
+++ NeuroAccessMaui/Extensions/SnifferExtensions.cs
@@ -11,23 +11,23 @@ namespace NeuroAccessMaui.Extensions
 		/// <summary>
 		/// Converts the latest Xmpp communication that the sniffer holds to plain text.
 		/// </summary>
-		/// <param name="sniffer">The sniffer whose contents to get.</param>
+		/// <param name="Sniffer">The sniffer whose contents to get.</param>
 		/// <returns>The xmpp communication in plain text.</returns>
-		public static async Task<string> SnifferToTextAsync(this InMemorySniffer sniffer)
+		public static async Task<string> SnifferToTextAsync(this InMemorySniffer Sniffer)
 		{
-			if (sniffer is null)
+			if (Sniffer is null)
 				return string.Empty;
 
 			StringBuilder sb = new();
 
-			await using StringWriter writer = new(sb);
-			await using TextWriterSniffer output = new(writer, BinaryPresentationMethod.ByteCount);
+			await using StringWriter Writer = new(sb);
+			await using TextWriterSniffer Output = new(Writer, BinaryPresentationMethod.ByteCount);
 
-			await sniffer.FlushAsync();
+			await Sniffer.FlushAsync();
 
-			sniffer.Replay(output);
+			Sniffer.Replay(Output);
 
-			await output.FlushAsync();
+			await Output.FlushAsync();
 
 			return sb.ToString();
 		}
