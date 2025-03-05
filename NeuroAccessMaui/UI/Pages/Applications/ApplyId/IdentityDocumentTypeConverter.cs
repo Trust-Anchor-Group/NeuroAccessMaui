777666c01a671b3adedfaeec5f038313e83f539d
Maximiliam Berggren <berggrenmille@hotmail.se>
2025-03-05 12:46:44 +0100

    PR: KyC Preparations among other things (#163)
    * Add experimental photos UI in ApplyId
    
    * Fix missing command parameter
    
    * Disable MSBUILD property in visual studio
    
    * Simplify method
    
    * Modify xaml structure & fix type hint
    
    * Add support for Front Back Images
    
    * Moved certain settings for organizational purposes
    
    * Code cleanup
    
    * Replace strings with localized resources
    
    * Centered ImageView
    
    * Add localization for ID verification in es, pt, en, sv
    
    Added new localization entries for identity document verification in Spanish, Portuguese, English, and Swedish resource files. These entries include titles, descriptions, and labels related to uploading and verifying identity documents such as passports, national ID cards, and driver's licenses. Specific instructions for uploading passport photos were also included.
    
    * Enhance identity document handling and photo display
    
    Added new converters for dynamic visibility and display of identity document sections. Enhanced the "Photos" section with a carousel view. Introduced new observable properties in `ApplyIdViewModel` for handling document types and photos. Updated methods for taking, picking, and processing photos with consistent variable naming and async handling. Added new classes for custom conversion logic of identity document types. Improved overall user experience and interface usability.
    
    * Fix fonts not loading on emulators
    
    * Remove duplicate localization
    
    * Update logos and icons
    
    * Refactor Create Account step.
    
    Fixes handling of errors and rejections
    
    * Add localized string
    
    * Add new image
    
    * Add localization
    
    * Update logic for handling ClientMessages
    
    If localization code was not found then we use the default message
    
    * Add Passport geometry
    
    * Update desgin in applyIdPage
    
    Added new text and overlay for passport
    
    * Update names of attachments
    
    * Modified text in Images popup
    
    * Add error handling
    
    * Fix static command bug
    
    * Refactor CheckPasswordPopup and Fix bug when dismiss by pressing background
    
    * Add support for BlurredBackground
    
    * Add missing localization
    
    ---------
    
    Co-authored-by: Maximiliam Berggren <maximiliam.berggren@trustanchorgroup.com>

---
+++ NeuroAccessMaui/UI/Pages/Applications/ApplyId/IdentityDocumentTypeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuroAccessMaui.Resources.Languages;
+using NeuroAccessMaui.Services;
+
+namespace NeuroAccessMaui.UI.Pages.Applications.ApplyId
+{
+	public class IdentityDocumentTypeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is IdentityDocumentType Type)
+			{
+				return Type switch
+				{
+					IdentityDocumentType.None => ServiceRef.Localizer[nameof(AppResources.None)], 
+					IdentityDocumentType.Passport => ServiceRef.Localizer[nameof(AppResources.Passport)],
+					IdentityDocumentType.NationalId => ServiceRef.Localizer[nameof(AppResources.NationalId)],
+					IdentityDocumentType.DriverLicense => ServiceRef.Localizer[nameof(AppResources.DriverLicense)],
+					_ => Type.ToString()
+				};
+			}
+			return value;
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
