022a6d3bedc58eb809680450747a31170fdf2bb0
berggrenmille <berggrenmille@hotmail.se>
2025-03-17 02:11:15 +0100

    Add ConditionView and SwitchCaseView<T> custom controls
    Introduce ConditionView and SwitchCaseView<T> in NeuroAccessMaui.UI.Controls namespace.
    
    - ConditionView: Displays one of two views based on a boolean condition with bindable properties: FalseProperty, TrueProperty, and ConditionProperty.
    - SwitchCaseView<T>: Displays one of multiple views based on a matching case value with bindable properties: CasesProperty, DefaultProperty, and SwitchProperty.
    - CaseView<T>: Represents a single case with a specific value using the CaseProperty.
    
    These enhancements provide flexible and reusable components for conditionally displaying views.

---
+++ NeuroAccessMaui/UI/Controls/ConditionView.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroAccessMaui.UI.Controls
+{
+    /// <summary>
+    /// A custom ContentView that displays one of two views based on a boolean condition.
+    /// </summary>
+    internal class ConditionView : ContentView
+    {
+        /// <summary>
+        /// Identifies the <see cref="False"/> bindable property.
+        /// </summary>
+        public static readonly BindableProperty FalseProperty = BindableProperty.Create(nameof(False), typeof(View), typeof(ConditionView), null);
+
+        /// <summary>
+        /// Identifies the <see cref="True"/> bindable property.
+        /// </summary>
+        public static readonly BindableProperty TrueProperty = BindableProperty.Create(nameof(True), typeof(View), typeof(ConditionView), null);
+
+        /// <summary>
+        /// Identifies the <see cref="Condition"/> bindable property.
+        /// </summary>
+        public static readonly BindableProperty ConditionProperty = BindableProperty.Create(nameof(Condition), typeof(bool), typeof(ConditionView), false, propertyChanged: ConditionChanged);
+
+        /// <summary>
+        /// Called when the <see cref="Condition"/> property changes.
+        /// </summary>
+        /// <param name="Bindable">The bindable object.</param>
+        /// <param name="OldValue">The old value of the property.</param>
+        /// <param name="NewValue">The new value of the property.</param>
+        private static void ConditionChanged(BindableObject Bindable, object OldValue, object NewValue)
+        {
+            ConditionView ConditionView = (ConditionView)Bindable;
+            ConditionView.Content = (bool)NewValue ? ConditionView.True : ConditionView.False;
+        }
+
+        /// <summary>
+        /// Gets or sets the condition that determines which view to display.
+        /// </summary>
+        public bool Condition
+        {
+            get => (bool)this.GetValue(ConditionProperty);
+            set => this.SetValue(ConditionProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the view to display when <see cref="Condition"/> is true.
+        /// </summary>
+        public View True
+        {
+            get => (View)this.GetValue(TrueProperty);
+            set => this.SetValue(TrueProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the view to display when <see cref="Condition"/> is false.
+        /// </summary>
+        public View False
+        {
+            get => (View)this.GetValue(FalseProperty);
+            set => this.SetValue(FalseProperty, value);
+        }
+    }
+}
