967edf3683de74df088ac8666313c6e10f57ba40
berggrenmille <berggrenmille@hotmail.se>
2025-04-02 01:00:16 +0200

    Add Notifications page with ViewModel and UI layout
    Created NotificationsPage.xaml to define the UI layout for the Notifications page, including a grid layout with a back button, title label, and a CollectionView for displaying notifications.
    
    Added NotificationsPage.xaml.cs to set the BindingContext to an instance of NotificationsViewModel.
    
    Added NotificationsViewModel.cs to manage notifications data and handle related events, including methods for lifecycle events and handling new notification events.

---
+++ NeuroAccessMaui/UI/Pages/Notifications/NotificationsViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using NeuroAccessMaui.Services;
+using NeuroAccessMaui.Services.Notification;
+using NeuroAccessMaui.UI.MVVM;
+
+namespace NeuroAccessMaui.UI.Pages.Notifications
+{
+	public partial class NotificationsViewModel : BaseViewModel
+	{
+
+		public ObservableTask NotificationsLoader = new();
+
+		[ObservableProperty]
+		private ObservableCollection<NotificationEvent> notifications = new();
+
+
+		protected override Task OnAppearing()
+		{
+			ServiceRef.NotificationService.OnNewNotification += this.OnNotificationEvent;
+			//Load notifications
+			this.NotificationsLoader.Load(async () =>
+			{
+				NotificationEvent[] NotificationEvents = ServiceRef.NotificationService.GetAllEvents();
+				await MainThread.InvokeOnMainThreadAsync(() =>
+				{
+					this.Notifications.Clear();
+					foreach (NotificationEvent Event in NotificationEvents)
+						this.Notifications.Add(Event);
+				});
+			});
+			return base.OnAppearing();
+		}
+
+		protected override Task OnDisappearing()
+		{
+			ServiceRef.NotificationService.OnNewNotification -= this.OnNotificationEvent;
+
+			return base.OnDisappearing();
+		}
+
+		//On notification event
+		public async Task OnNotificationEvent(object sender, NotificationEventArgs e)
+		{
+			//Wait for notification to load first
+			await this.NotificationsLoader.WaitAllAsync(true);
+
+			//if notification is not already in the list, add it
+			///TODO: FIX this check
+			if (!this.Notifications.Any(n => n.ObjectId == e.Event.ObjectId))
+			{
+				await MainThread.InvokeOnMainThreadAsync(() =>
+				{
+					this.Notifications.Add(e.Event);
+				});
+			}
+		}
+
+	}
+}
