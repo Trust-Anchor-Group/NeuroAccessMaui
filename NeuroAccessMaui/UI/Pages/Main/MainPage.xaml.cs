ef0def3554fbd5a99f603be7980607bc972e4592
Maximiliam Berggren <maximiliam.berggren@trustanchorgroup.com>
2025-04-15 13:01:39 +0200

    Remove test code

--- NeuroAccessMaui/UI/Pages/Main/MainPage.xaml.cs
+++ NeuroAccessMaui/UI/Pages/Main/MainPage.xaml.cs
@@ -8,11 +8,6 @@ namespace NeuroAccessMaui.UI.Pages.Main
 		{
 			this.InitializeComponent();
 			this.ContentPageModel = ViewModel;
-
-			ServiceRef.PlatformSpecific.ShowIdentitiesNotification("Test", "Hello world", new Dictionary<string, string>
-			{
-				{ "Test", "Value" }
-			});
 		}
 
 		private async void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
