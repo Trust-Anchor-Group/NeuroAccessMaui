868331bb25723bdab325717c55ab15ee327427ec
berggrenmille <berggrenmille@hotmail.se>
2025-05-14 00:19:11 +0200

    Add IThemeService and ThemeService for theme management
    Introduced a new `IThemeService` interface for managing themes,

---
+++ NeuroAccessMaui/Services/Theme/IThemeService.cs
@@ -0,0 +1,32 @@
+﻿using Waher.Runtime.Inventory;
+
+namespace NeuroAccessMaui.Services.Theme
+{
+	/// <summary>
+	/// Service for loading and applying themes/branding in the app.
+	/// </summary>
+	[DefaultImplementation(typeof(ThemeService))]
+	public interface IThemeService
+	{
+		/// <summary>
+		/// Loads and applies the provider theme (if exists).
+		/// </summary>
+		Task ApplyProviderTheme();
+
+		
+
+		/// <summary>
+		/// Sets the theme for the app.
+		/// </summary>
+		/// <param name="Type">The theme type</param>
+		Task SetTheme(AppTheme Type);
+
+		/// <summary>
+		/// Gets the current theme for the app.
+		/// </summary>
+		/// <returns></returns>
+		Task<AppTheme> GetTheme();
+
+	}
+
+}
