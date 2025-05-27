db242dd3879bca8e6c8fd884891960f0889f96b9
Peter Waher <peterwaher@hotmail.com>
2025-05-26 21:25:39 -0400

    Minor edit

--- NeuroAccessMaui/Services/Xmpp/XmppService.cs
+++ NeuroAccessMaui/Services/Xmpp/XmppService.cs
@@ -1356,9 +1356,7 @@ namespace NeuroAccessMaui.Services.Xmpp
 		private async Task TransferIdDelivered(object? Sender, MessageEventArgs e)
 		{
 			if (e.From != Constants.Domains.OnboardingDomain)
-			{
 				return;
-			}
 
 			string Code = XML.Attribute(e.Content, "code");
 			bool Deleted = XML.Attribute(e.Content, "deleted", false);
