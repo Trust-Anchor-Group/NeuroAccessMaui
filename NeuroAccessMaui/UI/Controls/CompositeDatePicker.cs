51cd8318837ca0777c7e5e39aed7e1fdaf6cc230
berggrenmille <berggrenmille@hotmail.se>
2025-06-18 02:20:52 +0200

    Refactor composite date picker control

--- NeuroAccessMaui/UI/Controls/CompositeDatePicker.cs
+++ NeuroAccessMaui/UI/Controls/CompositeDatePicker.cs
@@ -1,254 +1,284 @@
-﻿using NeuroAccessMaui.UI.Controls.Extended;
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using NeuroAccessMaui.UI.Controls.Extended;
 using NeuroAccessMaui.UI.Converters;
 using Waher.Events;
 
 namespace NeuroAccessMaui.UI.Controls
 {
 	/// <summary>
-	///  Extended DatePicker for nullable values with text placeholder
+	/// Extended DatePicker for nullable values with text placeholder.
 	/// </summary>
 	public class CompositeDatePicker : CompositeInputView
 	{
 		/// <summary>
-		/// The font property
+		/// The Font property.
 		/// </summary>
-		public static readonly BindableProperty FontProperty =
-			 BindableProperty.Create(nameof(Font), typeof(Microsoft.Maui.Font), typeof(CompositePicker), new Microsoft.Maui.Font());
+		public static readonly BindableProperty FontProperty = BindableProperty.Create(
+			propertyName: nameof(Font),
+			returnType: typeof(Microsoft.Maui.Font),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: new Microsoft.Maui.Font());
 
 		/// <summary>
-		/// Gets or sets the Font
+		/// Gets or sets the font.
 		/// </summary>
 		public Microsoft.Maui.Font Font
 		{
-			get { return (Microsoft.Maui.Font)this.GetValue(FontProperty); }
-			set { this.SetValue(FontProperty, value); }
+			get => (Microsoft.Maui.Font)GetValue(FontProperty);
+			set => SetValue(FontProperty, value);
 		}
 
 		/// <summary>
-		/// The XAlign property
+		/// The XAlign property.
 		/// </summary>
-		public static readonly BindableProperty XAlignProperty =
-			 BindableProperty.Create(nameof(XAlign), typeof(TextAlignment), typeof(CompositePicker),
-			 TextAlignment.Start);
+		public static readonly BindableProperty XAlignProperty = BindableProperty.Create(
+			propertyName: nameof(XAlign),
+			returnType: typeof(TextAlignment),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: TextAlignment.Start);
 
 		/// <summary>
-		/// Gets or sets the X alignment of the text
+		/// Gets or sets the horizontal alignment of the text.
 		/// </summary>
 		public TextAlignment XAlign
 		{
-			get { return (TextAlignment)this.GetValue(XAlignProperty); }
-			set { this.SetValue(XAlignProperty, value); }
+			get => (TextAlignment)GetValue(XAlignProperty);
+			set => SetValue(XAlignProperty, value);
 		}
 
 		/// <summary>
-		/// The HasBorder property
+		/// The HasBorder property.
 		/// </summary>
-		public static readonly BindableProperty HasBorderProperty =
-			 BindableProperty.Create(nameof(HasBorder), typeof(bool), typeof(CompositePicker), true);
+		public static readonly BindableProperty HasBorderProperty = BindableProperty.Create(
+			propertyName: nameof(HasBorder),
+			returnType: typeof(bool),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: true);
 
 		/// <summary>
-		/// Gets or sets if the border should be shown or not
+		/// Gets or sets whether the border should be shown.
 		/// </summary>
 		public bool HasBorder
 		{
-			get { return (bool)this.GetValue(HasBorderProperty); }
-			set { this.SetValue(HasBorderProperty, value); }
+			get => (bool)GetValue(HasBorderProperty);
+			set => SetValue(HasBorderProperty, value);
 		}
 
 		/// <summary>
-		/// The Placeholder property
+		/// The Placeholder property.
 		/// </summary>
-		public static readonly BindableProperty PlaceholderProperty =
-			 BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(CompositePicker), string.Empty, BindingMode.OneWay);
+		public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(
+			propertyName: nameof(Placeholder),
+			returnType: typeof(string),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: string.Empty,
+			defaultBindingMode: BindingMode.OneWay);
 
 		/// <summary>
-		/// Get or sets the PlaceHolder
+		/// Gets or sets the placeholder text to display when no date is selected.
 		/// </summary>
 		public string Placeholder
 		{
-			get { return (string)this.GetValue(PlaceholderProperty); }
-			set { this.SetValue(PlaceholderProperty, value); }
+			get => (string)GetValue(PlaceholderProperty);
+			set => SetValue(PlaceholderProperty, value);
 		}
 
 		/// <summary>
-		/// The PlaceholderTextColor property
+		/// The PlaceholderTextColor property.
 		/// </summary>
-		public static readonly BindableProperty PlaceholderTextColorProperty =
-			 BindableProperty.Create(nameof(PlaceholderTextColor), typeof(Color), typeof(CompositePicker), Colors.Transparent);
+		public static readonly BindableProperty PlaceholderTextColorProperty = BindableProperty.Create(
+			propertyName: nameof(PlaceholderTextColor),
+			returnType: typeof(Color),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: Colors.Transparent);
 
 		/// <summary>
-		/// Sets color for placeholder text
+		/// Gets or sets the color of the placeholder text.
 		/// </summary>
 		public Color PlaceholderTextColor
 		{
-			get { return (Color)this.GetValue(PlaceholderTextColorProperty); }
-			set { this.SetValue(PlaceholderTextColorProperty, value); }
+			get => (Color)GetValue(PlaceholderTextColorProperty);
+			set => SetValue(PlaceholderTextColorProperty, value);
 		}
 
 		/// <summary>
-		/// The TextColor property
+		/// The TextColor property.
 		/// </summary>
-		public static readonly BindableProperty TextColorProperty =
-			BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(CompositePicker), Colors.Black);
+		public static readonly BindableProperty TextColorProperty = BindableProperty.Create(
+			propertyName: nameof(TextColor),
+			returnType: typeof(Color),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: Colors.Black);
 
 		/// <summary>
-		/// Gets or sets the TextColor
+		/// Gets or sets the color of the selected date text.
 		/// </summary>
 		public Color TextColor
 		{
-			get => (Color)this.GetValue(TextColorProperty);
-			set => this.SetValue(TextColorProperty, value);
+			get => (Color)GetValue(TextColorProperty);
+			set => SetValue(TextColorProperty, value);
 		}
 
 		/// <summary>
-		/// Bindable property for the style applied to the picker
+		/// The PickerStyle property.
 		/// </summary>
-		public static readonly BindableProperty PickerStyleProperty =
-			BindableProperty.Create(nameof(PickerStyle), typeof(Style), typeof(CompositePicker), null);
+		public static readonly BindableProperty PickerStyleProperty = BindableProperty.Create(
+			propertyName: nameof(PickerStyle),
+			returnType: typeof(Style),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: null);
 
+		/// <summary>
+		/// Gets or sets the style applied to the internal DatePicker.
+		/// </summary>
 		public Style PickerStyle
 		{
-			get => (Style)this.GetValue(PickerStyleProperty);
-			set => this.SetValue(PickerStyleProperty, value);
+			get => (Style)GetValue(PickerStyleProperty);
+			set => SetValue(PickerStyleProperty, value);
 		}
 
 		/// <summary>
-		/// The NullableDate property
+		/// The NullableDate property.
 		/// </summary>
-		public static readonly BindableProperty NullableDateProperty =
-			 BindableProperty.Create(nameof(NullableDate), typeof(DateTime?), typeof(CompositeDatePicker), null, BindingMode.TwoWay,
-			 propertyChanged: DatePropertyChanged);
+		public static readonly BindableProperty NullableDateProperty = BindableProperty.Create(
+			propertyName: nameof(NullableDate),
+			returnType: typeof(DateTime?),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: null,
+			defaultBindingMode: BindingMode.TwoWay);
 
 		/// <summary>
-		/// The MinimumDate property
-		/// </summary>
-		public static readonly BindableProperty MinimumDateProperty =
-			BindableProperty.Create(nameof(MinimumDate), typeof(DateTime), typeof(CompositeDatePicker), DateTime.MinValue);
-
-		/// <summary>
-		/// The MaximumDate property
-		/// </summary>
-		public static readonly BindableProperty MaximumDateProperty =
-			BindableProperty.Create(nameof(MaximumDate), typeof(DateTime), typeof(CompositeDatePicker), DateTime.MaxValue);
-
-		/// <summary>
-		/// The Date property
-		/// </summary>
-		public static readonly BindableProperty DateProperty =
-			BindableProperty.Create(nameof(Date), typeof(DateTime), typeof(CompositeDatePicker), DateTime.Now, propertyChanged: DatePropertyChanged);
-
-		/// <summary>
-		/// Gets or sets the MinimumDate
-		/// </summary>
-		public DateTime MinimumDate
-		{
-			get => (DateTime)this.GetValue(MinimumDateProperty);
-			set => this.SetValue(MinimumDateProperty, value);
-		}
-
-		/// <summary>
-		/// Gets or sets the MaximumDate
-		/// </summary>
-		public DateTime MaximumDate
-		{
-			get => (DateTime)this.GetValue(MaximumDateProperty);
-			set => this.SetValue(MaximumDateProperty, value);
-		}
-
-		/// <summary>
-		/// Gets or sets the Date
-		/// </summary>
-		public DateTime Date
-		{
-			get => (DateTime)this.GetValue(DateProperty);
-			set => this.SetValue(DateProperty, value);
-		}
-
-		/// <summary>
-		/// Get or sets the NullableDate
+		/// Gets or sets the selected date, or null if none.
 		/// </summary>
 		public DateTime? NullableDate
 		{
-			get { return (DateTime?)this.GetValue(NullableDateProperty); }
-			set
-			{
-				if (value != this.NullableDate)
-				{
-					this.SetValue(NullableDateProperty, value);
-					this.UpdateDate();
-				}
-			}
+			get => (DateTime?)GetValue(NullableDateProperty);
+			set => SetValue(NullableDateProperty, value);
 		}
 
 		/// <summary>
-		/// Event sent when the date is changed.
+		/// The MinimumDate property.
+		/// </summary>
+		public static readonly BindableProperty MinimumDateProperty = BindableProperty.Create(
+			propertyName: nameof(MinimumDate),
+			returnType: typeof(DateTime),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: DateTime.MinValue);
+
+		/// <summary>
+		/// Gets or sets the minimum allowable date.
+		/// </summary>
+		public DateTime MinimumDate
+		{
+			get => (DateTime)GetValue(MinimumDateProperty);
+			set => SetValue(MinimumDateProperty, value);
+		}
+
+		/// <summary>
+		/// The MaximumDate property.
+		/// </summary>
+		public static readonly BindableProperty MaximumDateProperty = BindableProperty.Create(
+			propertyName: nameof(MaximumDate),
+			returnType: typeof(DateTime),
+			declaringType: typeof(CompositeDatePicker),
+			defaultValue: DateTime.MaxValue);
+
+		/// <summary>
+		/// Gets or sets the maximum allowable date.
+		/// </summary>
+		public DateTime MaximumDate
+		{
+			get => (DateTime)GetValue(MaximumDateProperty);
+			set => SetValue(MaximumDateProperty, value);
+		}
+
+		/// <summary>
+		/// Occurs when the nullable date is changed by user action.
 		/// </summary>
 		public event EventHandler<NullableDateChangedEventArgs>? NullableDateSelected;
 
-		private readonly DatePicker picker;
+		private readonly DatePicker Picker;
 
 		/// <summary>
-		/// Creates a new instance of the <see cref="CompositeDatePicker"/> class.
+		/// Initializes a new instance of the <see cref="CompositeDatePicker"/> class.
 		/// </summary>
 		public CompositeDatePicker()
 		{
-			this.picker = new DatePicker();
+			Picker = new DatePicker();
+			SetDefaultDate();
 
-			this.SetDefaultDate();
+			// Bind internal properties to this control
+			Picker.SetBinding(DatePicker.DateProperty,
+				new Binding(nameof(NullableDate), source: this, mode: BindingMode.TwoWay, converter: new NullableDateTimeConverter()));
+			Picker.SetBinding(DatePicker.MinimumDateProperty,
+				new Binding(nameof(MinimumDate), source: this));
+			Picker.SetBinding(DatePicker.MaximumDateProperty,
+				new Binding(nameof(MaximumDate), source: this));
+			Picker.SetBinding(DatePicker.TextColorProperty,
+				new Binding(nameof(TextColor), source: this));
+			Picker.SetBinding(DatePicker.StyleProperty,
+				new Binding(nameof(PickerStyle), source: this));
 
-			this.picker.SetBinding(DatePicker.DateProperty, new Binding(nameof(this.NullableDate), BindingMode.TwoWay, new NullableDateTimeConverter(), source: this));
-			this.picker.SetBinding(DatePicker.MinimumDateProperty, new Binding(nameof(this.MinimumDate), source: this));
-			this.picker.SetBinding(DatePicker.MaximumDateProperty, new Binding(nameof(this.MaximumDate), source: this));
-			this.picker.SetBinding(DatePicker.TextColorProperty, new Binding(nameof(this.TextColor), source: this));
-			this.picker.SetBinding(DatePicker.StyleProperty, new Binding(nameof(this.PickerStyle), source: this));
-			this.CenterView = this.picker;
+			// Apply initial format (placeholder or date)
+			UpdateFormat();
+
+			// Handle user changes and property updates
+			Picker.DateSelected += OnPickerDateSelected;
+			this.PropertyChanged += OnControlPropertyChanged;
+
+			CenterView = Picker;
 		}
 
-		static void DatePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		/// <summary>
+		/// Responds to changes in Placeholder or NullableDate to update the displayed format.
+		/// </summary>
+		private void OnControlPropertyChanged(object? Sender, PropertyChangedEventArgs E)
 		{
-			CompositeDatePicker Picker = (CompositeDatePicker)bindable;
-			EventHandler<NullableDateChangedEventArgs>? selected = Picker.NullableDateSelected;
-
-			selected.Raise(Picker, new NullableDateChangedEventArgs((DateTime?)oldValue, (DateTime?)newValue));
+			if (E.PropertyName == nameof(Placeholder) || E.PropertyName == nameof(NullableDate))
+				UpdateFormat();
 		}
 
-		private bool isDefaultDateSet = false;
-
-		/// <inheritdoc/>
-		protected override void OnPropertyChanged(string? propertyName = null)
+		/// <summary>
+		/// Handles the internal DatePicker's DateSelected event.
+		/// Syncs <see cref="NullableDate"/> and raises <see cref="NullableDateSelected"/>.
+		/// </summary>
+		private void OnPickerDateSelected(object? Sender, DateChangedEventArgs E)
 		{
-			base.OnPropertyChanged(propertyName);
-
-			if (propertyName == IsFocusedProperty.PropertyName)
-				// we don't know if the date picker was closed by the Ok or Cancel button,
-				// so we presuppose it was an Ok.
-				if (!this.IsFocused)
-					this.OnPropertyChanged(nameof(this.picker.DateProperty));
-
-			if (propertyName == nameof(this.picker.DateProperty) && !this.isDefaultDateSet)
-				this.NullableDate = this.picker.Date;
-
-			if (propertyName == NullableDateProperty.PropertyName)
-				if (this.NullableDate.HasValue)
-					this.picker.Date = this.NullableDate.Value;
+			var OldDate = NullableDate;
+			NullableDate = E.NewDate;
+			NullableDateSelected?.Invoke(this, new NullableDateChangedEventArgs(OldDate, NullableDate));
 		}
 
-		private void UpdateDate()
+		/// <summary>
+		/// Updates the <see cref="Picker.Format"/> property based on whether a date is selected.
+		/// </summary>
+		private void UpdateFormat()
 		{
-			if (this.NullableDate.HasValue)
-				this.picker.Date = this.NullableDate.Value;
+			if (NullableDate.HasValue)
+			{
+				// Use the current culture's short date pattern once a date is chosen.
+				Picker.Format = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
+			}
 			else
 			{
-				this.isDefaultDateSet = true;
-				this.SetDefaultDate();
-				this.isDefaultDateSet = false;
+				// Escape any single-quotes in the placeholder,
+				// then wrap the whole thing in single-quotes.
+				string escaped = Placeholder.Replace("'", "''");
+				Picker.Format = $"'{escaped}'";
 			}
 		}
 
+		/// <summary>
+		/// Initializes the internal picker to today's date without a time component.
+		/// </summary>
 		private void SetDefaultDate()
 		{
-			DateTime now = DateTime.Now;
-			this.picker.Date = new DateTime(now.Year, now.Month, now.Day);
+			var Now = DateTime.Now;
+			Picker.Date = new DateTime(Now.Year, Now.Month, Now.Day);
 		}
 	}
 }
