177101777c879951848f9729b91803c056354206
berggrenmille <berggrenmille@hotmail.se>
2025-06-30 01:33:41 +0200

    Add DateTimeToLastUpdatedConverter for date formatting
    Introduces a new `DateTimeToLastUpdatedConverter` class that implements the `IValueConverter` interface. This converter formats a `DateTime` value into a user-friendly string indicating how long ago the date occurred. It handles cases for recent dates (in minutes or hours) and older dates (short date format). If the input is not a `DateTime`, it returns a localized "Never" string. The `ConvertBack` method is defined but not implemented.

---
+++ NeuroAccessMaui/UI/Converters/DateTimeToLastUpdated.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Globalization;
+using NeuroAccessMaui.Resources.Languages;
+using NeuroAccessMaui.Services;
+
+namespace NeuroAccessMaui.UI.Converters
+{
+	public class DateTimeToLastUpdatedConverter : IValueConverter
+	{
+		public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if (value is not DateTime DtNullable)
+				return ServiceRef.Localizer[nameof(AppResources.Never)].Value.ToLower(culture);
+
+			DateTime Dt = DtNullable.ToUniversalTime(); // ensure local time
+			DateTime Now = DateTime.UtcNow;
+			TimeSpan Span = Now - Dt;
+
+			string TimeString;
+
+			if (Span.TotalMinutes < 1)
+				TimeString = ServiceRef.Localizer[nameof(AppResources.Now)].Value.ToLower(culture);
+			else if (Span.TotalHours < 1)
+				TimeString = ServiceRef.Localizer[nameof(AppResources.MinutesAgoFormat), false, (int)Span.TotalMinutes];
+			else if (Span.TotalDays < 1)
+				TimeString = ServiceRef.Localizer[nameof(AppResources.HoursAgoFormat), false, (int)Span.TotalHours];
+			else
+				TimeString = Dt.ToString("d", culture); // "d" = short date, respects locale
+			return ServiceRef.Localizer[nameof(AppResources.LastUpdatedFormat), false, TimeString];
+		}
+
+		public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+
+}
