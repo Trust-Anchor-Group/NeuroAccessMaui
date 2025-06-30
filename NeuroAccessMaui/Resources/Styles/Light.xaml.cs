266f927fd4876e1bc3321ccf6388373d2eb54449
berggrenmille <berggrenmille@hotmail.se>
2025-07-01 01:21:27 +0200

    Refactor color resources for light and dark themes
    Updated color definitions in `Colors.xaml`, removing and replacing several values for brand colors and button styles. Removed triggers in `CompositeInputViewStyles.xaml` related to input validation. Created new `Dark.xaml` and `Light.xaml` files with corresponding color definitions and initialization logic to support theme differentiation. Overall, these changes enhance the application's styling capabilities for both light and dark modes.

---
+++ NeuroAccessMaui/Resources/Styles/Light.xaml.cs
@@ -0,0 +1,12 @@
+namespace NeuroAccessMaui.Resources.Styles
+{
+
+	public partial class Light : ResourceDictionary
+	{
+		public Light()
+		{
+			this.InitializeComponent();
+			this["IsLocalThemeDictionary"] = true;
+		}
+	}
+}
