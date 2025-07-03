e075b107ab836cb5f9074e6a9b532d4cb0142d63
berggrenmille <berggrenmille@hotmail.se>
2025-07-03 23:11:37 +0200

    Add authentication service
    Implements a service for handling user authentication, including password input, biometric authentication, and user blocking.
    
    This service integrates with the tag profile and UI service to provide a secure and user-friendly authentication experience. It also includes features for checking user blocking status and validating passwords.

---
+++ NeuroAccessMaui/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroAccessMaui.Services.Authentication
+{
+	public interface IAuthenticationService
+	{
+		Task<string?> InputPasswordAsync(AuthenticationPurpose purpose);
+		Task<bool> AuthenticateUserAsync(AuthenticationPurpose purpose, bool force = false);
+		Task CheckUserBlockingAsync();
+		Task<bool> CheckPasswordAndUnblockUserAsync(string password);
+		// (Add other relevant authentication methods)
+	}
+
+}
