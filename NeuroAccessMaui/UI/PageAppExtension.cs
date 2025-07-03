46d3e42e0ec3b244c7fb23721491de4752c4fe62
berggrenmille <berggrenmille@hotmail.se>
2025-07-04 00:16:03 +0200

    Refactor registration of services
    Moves registration of pages and services to an extension method
    Services registers with 'Types' is now available in MAUI DI

--- NeuroAccessMaui/UI/PageAppExtension.cs
+++ NeuroAccessMaui/UI/PageAppExtension.cs
@@ -1,4 +1,21 @@
-﻿using CommunityToolkit.Maui;
+﻿using System.Reflection;
+using CommunityToolkit.Maui;
+using EDaler;
+using NeuroAccessMaui.Services.Cache.AttachmentCache;
+using NeuroAccessMaui.Services.Cache.InternetCache;
+using NeuroAccessMaui.Services.Contracts;
+using NeuroAccessMaui.Services.Crypto;
+using NeuroAccessMaui.Services.EventLog;
+using NeuroAccessMaui.Services.Intents;
+using NeuroAccessMaui.Services.Network;
+using NeuroAccessMaui.Services.Nfc;
+using NeuroAccessMaui.Services.Notification;
+using NeuroAccessMaui.Services.Settings;
+using NeuroAccessMaui.Services.Storage;
+using NeuroAccessMaui.Services.Tag;
+using NeuroAccessMaui.Services.Theme;
+using NeuroAccessMaui.Services.UI;
+using NeuroAccessMaui.Services.Xmpp;
 using NeuroAccessMaui.UI.Controls;
 using NeuroAccessMaui.UI.Pages;
 using NeuroAccessMaui.UI.Pages.Applications.Applications;
@@ -62,12 +79,119 @@ using NeuroAccessMaui.UI.Popups.Xmpp.ReportOrBlock;
 using NeuroAccessMaui.UI.Popups.Xmpp.ReportType;
 using NeuroAccessMaui.UI.Popups.Xmpp.SubscribeTo;
 using NeuroAccessMaui.UI.Popups.Xmpp.SubscriptionRequest;
+using NeuroFeatures;
+using Waher.Content;
+using Waher.Content.Images;
+using Waher.Content.Markdown;
+using Waher.Content.Xml;
+using Waher.Events;
+using Waher.Events.Persistence;
+using Waher.Networking.DNS;
+using Waher.Networking.XMPP;
+using Waher.Networking.XMPP.Avatar;
+using Waher.Networking.XMPP.Concentrator;
+using Waher.Networking.XMPP.Contracts;
+using Waher.Networking.XMPP.Control;
+using Waher.Networking.XMPP.Geo;
+using Waher.Networking.XMPP.HTTPX;
+using Waher.Networking.XMPP.Mail;
+using Waher.Networking.XMPP.P2P;
+using Waher.Networking.XMPP.P2P.E2E;
+using Waher.Networking.XMPP.PEP;
+using Waher.Networking.XMPP.Provisioning;
+using Waher.Networking.XMPP.PubSub;
+using Waher.Networking.XMPP.Push;
+using Waher.Networking.XMPP.Sensor;
+using Waher.Persistence;
+using Waher.Persistence.Files;
+using Waher.Persistence.Serialization;
+using Waher.Runtime.Geo;
+using Waher.Runtime.Inventory;
+using Waher.Runtime.Settings;
+using Waher.Script;
+using Waher.Script.Content;
+using Waher.Script.Graphs;
+using Waher.Security.JWS;
+using Waher.Security.JWT;
+using Waher.Things;
 
 namespace NeuroAccessMaui.UI
 {
 	public static class PageAppExtension
 	{
-		public static MauiAppBuilder RegisterPagesManager(this MauiAppBuilder Builder)
+		public static MauiAppBuilder RegisterTypes(this MauiAppBuilder Builder)
+		{
+			Assembly AppAssembly = typeof(App).Assembly;
+
+			if (!Types.IsInitialized)
+			{
+				Types.Initialize(
+					AppAssembly,
+					typeof(Database).Assembly,
+					typeof(ObjectSerializer).Assembly,
+					typeof(FilesProvider).Assembly,
+					typeof(RuntimeSettings).Assembly,
+					typeof(PersistedEvent).Assembly,
+					typeof(InternetContent).Assembly,
+					typeof(ImageCodec).Assembly,
+					typeof(MarkdownDocument).Assembly,
+					typeof(XML).Assembly,
+					typeof(DnsResolver).Assembly,
+					typeof(XmppClient).Assembly,
+					typeof(ContractsClient).Assembly,
+					typeof(NeuroFeaturesClient).Assembly,
+					typeof(EDalerClient).Assembly,
+					typeof(SensorClient).Assembly,
+					typeof(ControlClient).Assembly,
+					typeof(ConcentratorClient).Assembly,
+					typeof(ProvisioningClient).Assembly,
+					typeof(PubSubClient).Assembly,
+					typeof(PepClient).Assembly,
+					typeof(AvatarClient).Assembly,
+					typeof(PushNotificationClient).Assembly,
+					typeof(MailClient).Assembly,
+					typeof(GeoClient).Assembly,
+					typeof(GeoPosition).Assembly,
+					typeof(ThingReference).Assembly,
+					typeof(JwtFactory).Assembly,
+					typeof(JwsAlgorithm).Assembly,
+					typeof(Expression).Assembly,
+					typeof(Graph).Assembly,
+					typeof(GraphEncoder).Assembly,
+					typeof(XmppServerlessMessaging).Assembly,
+					typeof(HttpxClient).Assembly);
+			}
+
+			// Register exceptions as alerts.
+			Log.RegisterAlertExceptionType(true,
+				typeof(OutOfMemoryException),
+				typeof(StackOverflowException),
+				typeof(AccessViolationException),
+				typeof(InsufficientMemoryException));
+
+			EndpointSecurity.SetCiphers([typeof(Edwards448Endpoint)], false);
+
+			// Instantiate default services.
+			Builder.Services.AddSingleton<ITagProfile>((_)=>Types.InstantiateDefault<ITagProfile>(false));
+			Builder.Services.AddSingleton<ILogService>((_)=>Types.InstantiateDefault<ILogService>(false));
+			Builder.Services.AddSingleton<IUiService>((_)=>Types.InstantiateDefault<IUiService>(false));
+			Builder.Services.AddSingleton<ICryptoService>((_)=>Types.InstantiateDefault<ICryptoService>(false));
+			Builder.Services.AddSingleton<INetworkService>((_)=>Types.InstantiateDefault<INetworkService>(false));
+			Builder.Services.AddSingleton<IStorageService>((_)=>Types.InstantiateDefault<IStorageService>(false));
+			Builder.Services.AddSingleton<ISettingsService>((_)=>Types.InstantiateDefault<ISettingsService>(false));
+			Builder.Services.AddSingleton<IXmppService>((_)=>Types.InstantiateDefault<IXmppService>(false));
+			Builder.Services.AddSingleton<IAttachmentCacheService>((_)=>Types.InstantiateDefault<IAttachmentCacheService>(false));
+			Builder.Services.AddSingleton<IInternetCacheService>((_)=>Types.InstantiateDefault<IInternetCacheService>(false));
+			Builder.Services.AddSingleton<IContractOrchestratorService>((_)=>Types.InstantiateDefault<IContractOrchestratorService>(false));
+			Builder.Services.AddSingleton<INfcService>((_) => Types.InstantiateDefault<INfcService>(false));
+			Builder.Services.AddSingleton<INotificationService>((_) => Types.InstantiateDefault<INotificationService>(false));
+			Builder.Services.AddSingleton<IIntentService>((_) => Types.InstantiateDefault<IIntentService>(false));
+			Builder.Services.AddSingleton<IThemeService>((_) => Types.InstantiateDefault<IThemeService>(false));
+
+			return Builder;
+		}
+			
+		public static MauiAppBuilder RegisterPages(this MauiAppBuilder Builder)
 		{
 			// Applications
 			Builder.Services.AddTransient<ApplicationsPage, ApplicationsViewModel>();
