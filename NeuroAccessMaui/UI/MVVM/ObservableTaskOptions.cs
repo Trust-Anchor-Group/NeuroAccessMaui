5a5154f17259a63dab08b1da945f92ff2672ed02
berggrenmille <berggrenmille@hotmail.se>
2025-08-25 23:08:22 +0200

    Add ObservableTaskOptions class and refactor TaskRunner to use ObservableTaskStatus

---
+++ NeuroAccessMaui/UI/MVVM/ObservableTaskOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuroAccessMaui.UI.MVVM.Policies;
+using NeuroAccessMaui.UI.MVVM.Telemetry;
+
+namespace NeuroAccessMaui.UI.MVVM
+{
+	public sealed class ObservableTaskOptions
+	{
+		public string Name { get; set; } = "Operation";
+		public bool AutoStart { get; set; } = true;
+		public bool UseTaskRun { get; set; } = false;
+		public IObservableTaskTelemetry? Telemetry { get; set; }
+		public List<IAsyncPolicy> Policies { get; } = new();
+	}
+
+}
