1188876e72304b44f62d13e49ccf4e65a368c78a
berggrenmille <berggrenmille@hotmail.se>
2025-10-06 12:27:28 +0200

    Downgrade package versions
    - Downgrades Waher.Script, Waher.Content, EDaler, Waher.Content.Markdown, Waher.Content.Xml, Waher.Events, Waher.Events.XMPP, Waher.Networking.XMPP, Waher.Networking.XMPP.Avatar, Waher.Networking.XMPP.Concentrator, Waher.Networking.XMPP.Contracts, Waher.Networking.XMPP.Control, Waher.Networking.XMPP.P2P, Waher.Networking.XMPP.PEP, Waher.Persistence.FilesLW, Waher.Runtime.Inventory, Waher.Runtime.Profiling, Waher.Security.JWT packages.
    - Removes the optional parameter in EDalerClient.DecryptMessage method.

--- NeuroAccessMaui/Services/Xmpp/XmppService.cs
+++ NeuroAccessMaui/Services/Xmpp/XmppService.cs
@@ -4372,7 +4372,7 @@ namespace NeuroAccessMaui.Services.Xmpp
 		{
 			try
 			{
-				return await this.EDalerClient.DecryptMessage(EncryptedMessage, PublicKey, TransactionId, RemoteEndpoint, LocalIsRecipient);
+				return await this.EDalerClient.DecryptMessage(EncryptedMessage, PublicKey, TransactionId, RemoteEndpoint);
 			}
 			catch (Exception ex)
 			{
