cc29bce3b1186972c1f5b2ab6590d953b6738c3d
Tobias Hansson <tobias.hansson@trustanchorgroup.com>
2025-11-18 15:57:17 +0100

    Add help popup

---
+++ NeuroAccessMaui/UI/Popups/OnboardingHelp/OnboardingHelpViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NeuroAccessMaui.Resources.Languages;
+using NeuroAccessMaui.Services;
+
+namespace NeuroAccessMaui.UI.Popups.OnboardingHelp
+{
+	public partial class OnboardingHelpViewModel : BasePopupViewModel
+	{
+		public OnboardingHelpViewModel()
+		{
+			this.SupportEmail = "neuro-access@trustanchorgroup.com";
+		}
+
+		[ObservableProperty]
+		private string supportEmail;
+
+		[RelayCommand]
+		private async Task ContactSupport()
+		{
+			string Email = this.SupportEmail;
+			string Subject = ServiceRef.Localizer[nameof(AppResources.SupportEmailSubject)];
+
+			string MailtoUri = $"mailto:{Email}?subject={Uri.EscapeDataString(Subject)}";
+
+			try
+			{
+				if (!await Launcher.OpenAsync(new Uri(MailtoUri)))
+				{
+					await ServiceRef.UiService.DisplayAlert(
+						ServiceRef.Localizer[nameof(AppResources.ErrorTitle)],
+						ServiceRef.Localizer[nameof(AppResources.EmailClientNotAvailable), this.SupportEmail],
+						ServiceRef.Localizer[nameof(AppResources.Ok)]);
+				}
+			}
+			catch (Exception)
+			{
+				await ServiceRef.UiService.DisplayAlert(
+					 ServiceRef.Localizer[nameof(AppResources.ErrorTitle)],
+					 ServiceRef.Localizer[nameof(AppResources.EmailClientNotAvailable), this.SupportEmail],
+					 ServiceRef.Localizer[nameof(AppResources.Ok)]);
+			}
+		}
+	}
+}
