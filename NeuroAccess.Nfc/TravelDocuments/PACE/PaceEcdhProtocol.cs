e9d795d41522c238a9cd345a3a157add4e70f603
Peter Waher <peterwaher@hotmail.com>
2026-04-21 13:20:32 -0400

    Minor edits

--- NeuroAccess.Nfc/TravelDocuments/PACE/PaceEcdhProtocol.cs
+++ NeuroAccess.Nfc/TravelDocuments/PACE/PaceEcdhProtocol.cs
@@ -140,9 +140,8 @@ namespace NeuroAccess.Nfc.TravelDocuments.PACE
 			if (Seed is null)
 				return this.curve!.GenerateSecret();
 
-			int DigestSizeBits = checked(this.curve!.BigIntegerBytes * 8);
-			SHAKE256 H = new(DigestSizeBits);
-			byte[] B = new byte[this.curve!.BigIntegerBytes];
+			SHAKE256 H = new(this.curve!.BigIntegerBytes << 3);
+			byte[] B;
 			System.Numerics.BigInteger D;
 			System.Numerics.BigInteger? Order;
 
